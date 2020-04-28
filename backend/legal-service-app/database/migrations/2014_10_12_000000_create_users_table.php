36ae494a28154ae58fed44a6f087d9c7b9d901b8
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-29 00:02:05 +0200

    Added users migration

--- backend/legal-service-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/legal-service-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 255);
+            $table->string('surname', 255);
             $table->string('email')->unique();
+            $table->string('phone');
+            $table->boolean('is_active');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
             $table->rememberToken();
             $table->timestamps();
         });

ba6bc3b7ab8e7f836b493e0799b2b54398bbb691
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-29 00:48:54 +0200

    Removed receiver_id from messages migration

--- backend/legal-service-app/database/migrations/2020_04_28_224041_create_messages_table.php
+++ backend/legal-service-app/database/migrations/2020_04_28_224041_create_messages_table.php
@@ -16,7 +16,6 @@ class CreateMessagesTable extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('sender_id')->constrained('users');
-            $table->foreignId('receiver_id')->constrained('users');
             $table->string('content', 2048);
             $table->boolean('is_read')->default(false);
             $table->foreignId('chat_id')->constrained('chats');

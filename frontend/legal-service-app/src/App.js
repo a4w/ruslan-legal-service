46d3048cf7ec67ed8dac69f6a4fc06dc6b80a479
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-04-29 18:00:50 +0200

    Added header and footer html

--- frontend/legal-service-app/src/App.js
+++ frontend/legal-service-app/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./App.css";
 import Register from "./Register";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 function App() {
-  return <Register />;
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div>
+        <Register />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;

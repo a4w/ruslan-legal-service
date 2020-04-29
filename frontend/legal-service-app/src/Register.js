7ed7c29fe982db124f325441129c3c46e3ef6985
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-04-29 17:42:46 +0200

    Input with map

--- frontend/legal-service-app/src/Register.js
+++ frontend/legal-service-app/src/Register.js
@@ -1,70 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import "./assets/css/bootstrap-datetimepicker.min.css";
 import "./assets/css/bootstrap.min.css";
 import "./assets/css/bootstrap.css";
 import "./assets/css/style.css";
 
 const Register = () => {
+  const initUser = {
+    Name: "",
+    Surname: "",
+    Email: "",
+    Phone: "",
+    Password: "",
+  };
+  const [user, setUser] = useState(initUser);
+  const fields = ["Name", "Surname", "Email", "Phone", "Password"];
+  const OnChangeHandler = (event) => {
+    console.log("changing ", event.target.name);
+    setUser({ ...user, [event.target.name]: event.target.value });
+    console.log("user", user);
+  };
+  // <div>
+  //     {users.map((element) => (<Card key={element.id} {...element} />
+  //     ))}
+  //   </div>
   return (
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-8 offset-md-2">
-            <div class="account-content">
-              <div class="row align-items-center justify-content-center">
-                <div class="col-md-7 col-lg-6 login-left">
-                  <img
+    <div className="content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <div className="account-content">
+              <div className="row align-items-center justify-content-center">
+                <div className="col-md-7 col-lg-6 login-left">
+                  {/* <img
                     src="assets/img/login-banner.png"
-                    class="img-fluid"
+                    className="img-fluid"
                     alt="Doccure Register"
-                  />
+                  /> */}
                 </div>
-                <div class="col-md-12 col-lg-6 login-right">
-                  <div class="login-header">
+                <div className="col-md-12 col-lg-6 login-right">
+                  <div className="login-header">
                     <h3>
                       Patient Register{" "}
-                      <a href="doctor-register.html">Are you a Doctor?</a>
+                      {/* <a href="doctor-register.html">Are you a Doctor?</a> */}
                     </h3>
                   </div>
 
-                  <form action="doctor-dashboard.html">
-                    <div class="form-group form-focus">
-                      <input type="text" class="form-control floating" />
-                      <label class="focus-label">Name</label>
-                    </div>
-                    <div class="form-group form-focus">
-                      <input type="text" class="form-control floating" />
-                      <label class="focus-label">Mobile Number</label>
-                    </div>
-                    <div class="form-group form-focus">
-                      <input type="password" class="form-control floating" />
-                      <label class="focus-label">Create Password</label>
-                    </div>
-                    <div class="text-right">
-                      <a class="forgot-link" href="login.html">
+                  <form>
+                    {fields.map((element) => (
+                      <Input
+                        key={element}
+                        name={element}
+                        value={user[element]}
+                        type={element}
+                        OnChangeHandler={OnChangeHandler}
+                      />
+                    ))}
+                    <div className="text-right">
+                      {/* <a className="forgot-link" href="login.html">
                         Already have an account?
-                      </a>
+                      </a> */}
                     </div>
                     <button
-                      class="btn btn-primary btn-block btn-lg login-btn"
+                      className="btn btn-primary btn-block btn-lg login-btn"
                       type="submit"
                     >
                       Signup
                     </button>
-                    <div class="login-or">
-                      <span class="or-line"></span>
-                      <span class="span-or">or</span>
+                    <div className="login-or">
+                      <span className="or-line"></span>
+                      <span className="span-or">or</span>
                     </div>
-                    <div class="row form-row social-login">
-                      <div class="col-6">
-                        <a href="#" class="btn btn-facebook btn-block">
-                          <i class="fab fa-facebook-f mr-1"></i> Login
-                        </a>
+                    <div className="row form-row social-login">
+                      <div className="col-6">
+                        {/* <a href="#" className="btn btn-facebook btn-block">
+                          <i className="fab fa-facebook-f mr-1"></i> Login
+                        </a> */}
                       </div>
-                      <div class="col-6">
-                        <a href="#" class="btn btn-google btn-block">
-                          <i class="fab fa-google mr-1"></i> Login
-                        </a>
+                      <div className="col-6">
+                        {/* <a href="#" className="btn btn-google btn-block">
+                          <i className="fab fa-google mr-1"></i> Login
+                        </a> */}
                       </div>
                     </div>
                   </form>
@@ -78,4 +93,27 @@ const Register = () => {
   );
 };
 
+// submitEvent={submitEvent}
+//           placeholder={"password"}
+//           value={user.password}
+//           type={"password"}
+//           name={"password"}
+//           OnChangeHandler={OnChangeHandler}
+//           required={true}
+
+const Input = ({ value, name, OnChangeHandler, type }) => {
+  return (
+    <div className="form-group form-focus">
+      <input
+        name={name}
+        value={value}
+        type={type}
+        onChange={OnChangeHandler}
+        required
+        className="form-control floating"
+      />
+      <label className="focus-label">{name}</label>
+    </div>
+  );
+};
 export default Register;

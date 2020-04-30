62a40b4d5bd566387905f01da650c2cca54715bf
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-30 03:30:14 +0200

    Added Validations.js
    This file will contain all the validation rules for the application
    It currently contains the registrationValidation

---
+++ frontend/legal-service-app/src/Validations.js
@@ -0,0 +1,22 @@
+import { validate, test, enforce } from "vest";
+
+export const registrationValidation = (data) => {
+    return validate("RegistrationForm", () => {
+        console.log(data);
+        const trimmedEmail = truncate(data.email, 15);
+        test("email", `${trimmedEmail} is not valid email address`, () => {
+            enforce(data.email).isNotEmpty().matches(/[^@]+@[^.]+\..+/g);
+        });
+        test("password", "Password should be atleast 8 characters long", () => {
+            enforce(data.password).longerThanOrEquals(8);
+        });
+
+    });
+};
+
+const truncate = (string, len) => {
+    if(string.length > len)
+        return string.substring(0, len) + "...";
+    else
+        return string;
+};

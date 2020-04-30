2b3248a75095ea334912b3ed6762a5794ed99887
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-30 03:30:56 +0200

    Integrated validation in Register.js
    This integrates vest to Register.js and changes the style of Input
    componenet in order to not be restricted to 50px

--- frontend/legal-service-app/src/Register.js
+++ frontend/legal-service-app/src/Register.js
@@ -1,155 +1,169 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
+import React, {useState} from "react";
+import {registrationValidation} from "./Validations.js"
 import "./assets/css/bootstrap-datetimepicker.min.css";
 import "./assets/css/bootstrap.min.css";
 import "./assets/css/bootstrap.css";
 import "./assets/css/style.css";
 
 const Register = (props) => {
-  const initUser = {
-    name: "",
-    surName: "",
-    email: "",
-    number: "",
-    password: "",
-    isClient: true,
-  };
-  const [user, setUser] = useState(initUser);
-  const OnChangeHandler = (event) =>
-    setUser({ ...user, [event.target.name]: event.target.value });
-  const UserTypeHandler = () => {
-    setUser({ ...user, isClient: !user.isClient });
-  };
-  const OnSubmitHandler = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.target);
-  };
-  return (
-    <Wrapper>
-      <div className="login-header">
-        <h3>
-          {user.isClient ? "Client Register" : "Lawyer Register"}
-          <a href="#" onClick={UserTypeHandler}>
-            {user.isClient ? "Not a Client?" : "Not a Lawyer?"}
+    const initUser = {
+        name: "",
+        surName: "",
+        email: "",
+        number: "",
+        password: "",
+        isClient: true,
+    };
+
+    const [user, setUser] = useState(initUser);
+    const [validator, setValidator] = useState(null);
+
+    const OnChangeHandler = (event) =>{
+        setUser({...user, [event.target.name]: event.target.value});
+    }
+    const UserTypeHandler = () => {
+        setUser({...user, isClient: !user.isClient});
+    };
+
+    const OnSubmitHandler = (event) => {
+        event.preventDefault();
+        // const data = new FormData(event.target);
+        setValidator(registrationValidation({
+            email: user.email,
+            password: user.password
+        }));
+        if(validator.hasErrors())
+            return;
+        // Send request
+    };
+
+    return (
+        <Wrapper>
+            <div className="login-header">
+                <h3>
+                    {user.isClient ? "Client Register" : "Lawyer Register"}
+                    <a href="#" onClick={UserTypeHandler}>
+                        {user.isClient ? "Not a Client?" : "Not a Lawyer?"}
+                    </a>
+                </h3>
+            </div>
+            <form onSubmit={OnSubmitHandler}>
+                <Input
+                    placeholder={"Name"}
+                    name={"name"}
+                    value={user.name}
+                    type={"text"}
+                    OnChangeHandler={OnChangeHandler}
+                />
+                <Input
+                    placeholder={"Surname"}
+                    name={"surName"}
+                    value={user.surName}
+                    type={"text"}
+                    OnChangeHandler={OnChangeHandler}
+                />
+                <Input
+                    placeholder={"Telephone Number"}
+                    name={"number"}
+                    value={user.number}
+                    type={"text"}
+                    OnChangeHandler={OnChangeHandler}
+                />
+                <Input
+                    placeholder={"Email"}
+                    name={"email"}
+                    value={user.email}
+                    type={"text"}
+                    validator={validator}
+                    OnChangeHandler={OnChangeHandler}
+                />
+                <Input
+                    placeholder={"Password"}
+                    name={"password"}
+                    value={user.password}
+                    type={"password"}
+                    validator={validator}
+                    OnChangeHandler={OnChangeHandler}
+                />
+                <div className="text-right">
+                    <a className="forgot-link" href="login.html">
+                        Already have an account?
           </a>
-        </h3>
-      </div>
-      <form onSubmit={OnSubmitHandler}>
-        <Input
-          placeholder={"Name"}
-          name={"name"}
-          value={user.name}
-          type={"text"}
-          OnChangeHandler={OnChangeHandler}
-        />
-        <Input
-          placeholder={"Surname"}
-          name={"surName"}
-          value={user.surName}
-          type={"text"}
-          OnChangeHandler={OnChangeHandler}
-        />
-        <Input
-          placeholder={"Telephone Number"}
-          name={"number"}
-          value={user.number}
-          type={"text"}
-          OnChangeHandler={OnChangeHandler}
-        />
-        <Input
-          placeholder={"Email"}
-          name={"email"}
-          value={user.email}
-          type={"text"}
-          OnChangeHandler={OnChangeHandler}
-        />
-        <Input
-          placeholder={"Password"}
-          name={"password"}
-          value={user.password}
-          type={"password"}
-          OnChangeHandler={OnChangeHandler}
-        />
-        <div className="text-right">
-          <a className="forgot-link" href="login.html">
-            Already have an account?
-          </a>
-        </div>
-        <Button />
-        <div className="login-or">
-          <span className="or-line"></span>
-          <span className="span-or">or</span>
-        </div>
-        <div className="row form-row social-login">
-          <div className="col-6">
-            <a href="#" className="btn btn-facebook btn-block">
-              <i className="fab fa-facebook-f mr-1"></i> Login
+                </div>
+                <Button />
+                <div className="login-or">
+                    <span className="or-line"></span>
+                    <span className="span-or">or</span>
+                </div>
+                <div className="row form-row social-login">
+                    <div className="col-6">
+                        <a href="#" className="btn btn-facebook btn-block">
+                            <i className="fab fa-facebook-f mr-1"></i> Login
             </a>
-          </div>
-          <div className="col-6">
-            <a href="#" className="btn btn-google btn-block">
-              <i className="fab fa-google mr-1"></i> Login
+                    </div>
+                    <div className="col-6">
+                        <a href="#" className="btn btn-google btn-block">
+                            <i className="fab fa-google mr-1"></i> Login
             </a>
-          </div>
-        </div>
-      </form>
-    </Wrapper>
-  );
+                    </div>
+                </div>
+            </form>
+        </Wrapper>
+    );
 };
 const Wrapper = (props) => {
-  return (
-    <div className="content">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-8 offset-md-2">
-            <div className="account-content">
-              <div className="row align-items-center justify-content-center">
-                <div className="col-md-7 col-lg-6 login-left">
-                  <img
-                    src="./assets/img/login-banner.png"
-                    className="img-fluid"
-                    alt="Doccure Register"
-                  />
+    return (
+        <div className="content">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-8 offset-md-2">
+                        <div className="account-content">
+                            <div className="row align-items-center justify-content-center">
+                                <div className="col-md-7 col-lg-6 login-left">
+                                    <img
+                                        src="./assets/img/login-banner.png"
+                                        className="img-fluid"
+                                        alt="Doccure Register"
+                                    />
+                                </div>
+                                <div className="col-md-12 col-lg-6 login-right">
+                                    {props.children}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-12 col-lg-6 login-right">
-                  {props.children}
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-const Input = ({ placeholder, value, name, OnChangeHandler, type }) => {
-  return (
-    <div
-      className={
-        value === "" ? "form-group form-focus" : "form-group form-focus focused"
-      }
-    >
-      <input
-        name={name}
-        value={value}
-        type={type}
-        onChange={OnChangeHandler}
-        required
-        className="form-control floating"
-      />
-      <label className="focus-label">{placeholder}</label>
-    </div>
-  );
+const Input = ({validator, placeholder, value, name, OnChangeHandler, type}) => {
+    return (
+        <div
+            className={ value === "" ? "form-group form-focus" : "form-group form-focus focused" } >
+            <input
+                name={name}
+                value={value}
+                type={type}
+                onChange={OnChangeHandler}
+                required
+                className="form-control floating"
+            />
+            <label className="focus-label">{placeholder}</label>
+            {validator && validator.hasErrors(name) && <label className="text-danger ml-2 font-weight-light text-sm">{validator.getErrors(name)}</label>}
+        </div>
+    );
 };
 const Button = () => {
-  return (
-    <button
-      className="btn btn-primary btn-block btn-lg login-btn"
-      type="submit"
-    >
-      Signup
-    </button>
-  );
+    return (
+        <button
+            className="btn btn-primary btn-block btn-lg login-btn"
+            type="submit"
+        >
+            Signup
+        </button>
+    );
 };
 export default Register;

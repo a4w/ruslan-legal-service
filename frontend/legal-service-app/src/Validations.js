2e8b010c48bd5a0f56bf8a03ca9f523b5db7d22e
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-30 09:55:21 +0200

    Use vest build-in implementation in validation
    Using vest.only() method instead of constructing a touched array myself

--- frontend/legal-service-app/src/Validations.js
+++ frontend/legal-service-app/src/Validations.js
@@ -1,32 +1,32 @@
-import { validate, test, enforce } from "vest";
+import vest, { validate, test, enforce } from "vest";
 
-export const registrationValidation = (data, touched = false) => {
+export const registrationValidation = (data, field) => {
     return validate("RegistrationForm", () => {
-        if(!touched || touched.email){
-            const trimmedEmail = truncate(data.email.toString(), 15);
-            test("email", `${trimmedEmail} is not valid email address`, () => {
-                enforce(data.email.toString()).isNotEmpty().matches(/[^@]+@[^.]+\..+/g);
+        vest.only(field);
+
+        ["name", "surName", "number", "email", "password"].forEach((elem) => {
+            test(elem, "This field is required", () => {
+                enforce(data[elem].toString()).isNotEmpty();
             });
-        }
-        if(!touched || touched.password){
-            test("password", "Password should be atleast 8 characters long", () => {
-                enforce(data.password.toString()).longerThanOrEquals(8);
-            });
-        }
-        ["name", "surName", "number"].forEach((elem) => {
-            if(!touched || touched[elem]){
-                test(elem, "This field is required", () => {
-                    enforce(data[elem].toString()).isNotEmpty();
-                });
-            }
         });
+
+        const trimmedEmail = truncate(data.email.toString(), 15);
+        test("email", `${trimmedEmail} is not valid email address`, () => {
+            enforce(data.email.toString()).isNotEmpty().matches(/[^@]+@[^.]+\..+/g);
+        });
+
+        test("password", "Password should be atleast 8 characters long", () => {
+            enforce(data.password.toString()).longerThanOrEquals(8);
+        });
+
     });
 };
 
-export const defaultValidation = (_) => {
-    return validate("I do nothing", () => { });
+export const passingValidation = (_) => {
+    return validate("I always pass", () => { });
 };
 
+// TODO Move this to a helper utility or smth
 const truncate = (string, len) => {
     if(string.length > len)
         return string.substring(0, len) + "...";

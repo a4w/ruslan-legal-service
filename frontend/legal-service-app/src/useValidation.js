af9d35789790c3a4b85139f3229db659329f4467
Ahmed Wessam <33124826+ahmedWessamF@users.noreply.github.com>
2020-04-30 13:35:26 +0200

    Fix callback parameters

--- frontend/legal-service-app/src/useValidation.js
+++ frontend/legal-service-app/src/useValidation.js
@@ -7,18 +7,16 @@ const useValidation = (vest) => {
     const [errors, setErrors] = useState({});
     useEffect(()=>{
         let nextErrors = {};
-        let hasErrors = false;
         for(let tested of validation.tested){
             if(validation.getErrors(tested).length > 0){
                 nextErrors = {...nextErrors, [tested]: validation.getErrors(tested)};
-                hasErrors = true;
             }else{
                 delete nextErrors[tested];
             }
         }
         setErrors(nextErrors);
         if(typeof resolver.resolve === "function"){
-            resolver.resolve(hasErrors, nextErrors);
+            resolver.resolve(validation.hasErrors(), nextErrors);
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [validation]);
@@ -31,8 +29,8 @@ const useValidation = (vest) => {
         }
         return new Promise((resolve, _) => {
             setResolver({
-                resolve: (errors) => {
-                    resolve(errors);
+                resolve: (hasErrors, errors) => {
+                    resolve(hasErrors, errors);
                 }
             });
         });

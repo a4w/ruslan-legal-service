b776d93a82476ff6f3186efd095a8cd5fc340bf6
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-03 02:46:48 +0200

    Created login form validations

--- frontend/legal-service-app/src/Validations.js
+++ frontend/legal-service-app/src/Validations.js
@@ -12,24 +12,40 @@ export const registrationValidation = (data, field) => {
 
         const trimmedEmail = truncate(data.email.toString(), 15);
         test("email", `${trimmedEmail} is not valid email address`, () => {
-            enforce(data.email.toString()).isNotEmpty().matches(/[^@]+@[^.]+\..+/g);
+            enforce(data.email.toString())
+                .isNotEmpty()
+                .matches(/[^@]+@[^.]+\..+/g);
         });
 
         test("password", "Password should be atleast 8 characters long", () => {
             enforce(data.password.toString()).longerThanOrEquals(8);
         });
+    });
+};
 
+export const loginValidation = (data, field) => {
+    return validate("LoginForm", () => {
+        vest.only(field);
+        ["email", "password"].forEach((elem) => {
+            test(elem, "This field is required", () => {
+                enforce(data[elem].toString()).isNotEmpty();
+            });
+        });
+        const trimmedEmail = truncate(data.email.toString(), 15);
+        test("email", `${trimmedEmail} is not valid email address`, () => {
+            enforce(data.email.toString())
+                .isNotEmpty()
+                .matches(/[^@]+@[^.]+\..+/g);
+        });
     });
 };
 
 export const passingValidation = (_) => {
-    return validate("I always pass", () => { });
+    return validate("I always pass", () => {});
 };
 
 // TODO Move this to a helper utility or smth
 const truncate = (string, len) => {
-    if(string.length > len)
-        return string.substring(0, len) + "...";
-    else
-        return string;
+    if (string.length > len) return string.substring(0, len) + "...";
+    else return string;
 };

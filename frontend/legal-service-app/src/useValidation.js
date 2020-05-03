c8c42bf2202bec92a24c819697be57764ad3e387
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-03 02:47:39 +0200

    fixed validation bug

--- frontend/legal-service-app/src/useValidation.js
+++ frontend/legal-service-app/src/useValidation.js
@@ -1,41 +1,38 @@
-import {useState, useEffect} from "react";
-import {passingValidation} from "./Validations";
+import { useState, useEffect } from "react";
+import { passingValidation } from "./Validations";
 
 const useValidation = (vest) => {
     const [validation, setValidation] = useState(passingValidation);
-    const [resolver, setResolver] = useState({resolve: null});
+    const [resolver, setResolver] = useState({ resolve: null });
     const [errors, setErrors] = useState({});
-    useEffect(()=>{
-        let nextErrors = {};
-        for(let tested of validation.tested){
-            if(validation.getErrors(tested).length > 0){
-                nextErrors = {...nextErrors, [tested]: validation.getErrors(tested)};
-            }else{
-                delete nextErrors[tested];
-            }
+    useEffect(() => {
+        let nextErrors = errors;
+        for (let tested of validation.tested) {
+            nextErrors = { ...nextErrors, [tested]: validation.getErrors(tested) };
         }
+
         setErrors(nextErrors);
-        if(typeof resolver.resolve === "function"){
+        if (typeof resolver.resolve === "function") {
             resolver.resolve(validation.hasErrors(), nextErrors);
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [validation]);
 
     const run = (subject, field = false) => {
-        if(!field){
+        if (!field) {
             setValidation(vest(subject));
-        }else{
+        } else {
             setValidation(vest(subject, field));
         }
         return new Promise((resolve, _) => {
             setResolver({
                 resolve: (hasErrors, errors) => {
                     resolve(hasErrors, errors);
-                }
+                },
             });
         });
-    }
+    };
     return [errors, run];
-}
+};
 
 export default useValidation;

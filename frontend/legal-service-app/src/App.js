c4488cbd7e5f26ec7528b319d1432a30e0019184
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-03 02:47:57 +0200

    removed react fragment from app

--- frontend/legal-service-app/src/App.js
+++ frontend/legal-service-app/src/App.js
@@ -9,21 +9,19 @@ import { Route, BrowserRouter } from "react-router-dom";
 
 function App() {
     return (
-        <>
-            <BrowserRouter>
-                <NavBar />
-                <Route exact path='/'>
-                    <Home />
-                </Route>
-                <Route path='/login'>
-                    <Login />
-                </Route>
-                <Route path='/Register'>
-                    <Register />
-                </Route>
-                <Footer />
-            </BrowserRouter>
-        </>
+        <BrowserRouter>
+            <NavBar />
+            <Route exact path='/'>
+                <Home />
+            </Route>
+            <Route path='/login'>
+                <Login />
+            </Route>
+            <Route path='/Register'>
+                <Register />
+            </Route>
+            <Footer />
+        </BrowserRouter>
     );
 }
 

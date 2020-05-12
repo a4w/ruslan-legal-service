4f85930e0b774a8675a1d32a649bea0c34cf1195
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-05-12 06:08:48 +0200

    Update app name in example config

--- backend/legal-service-app/resources/views/emails/account/emailverification.blade.php
+++ backend/legal-service-app/resources/views/emails/account/emailverification.blade.php
@@ -1,12 +1,31 @@
-@component('mail::message')
-# Welcome to AppX
+@component('mail::layout')
 
-Thank you for registering! Please click the link below in order to verify your account
 
-@component('mail::button', ['url' => $verify_url])
+{{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+        <b>Lawbe</b>.co.uk
+        @endcomponent
+    @endslot
+
+# Welcome to Lawbe, The lawyer's platform
+
+## Thank you for registering!
+
+Please click the link below in order to verify your account
+
+@component('mail::button', ['url' => $verify_url ?? ''])
 Verify
 @endcomponent
 
 Thanks,<br>
 {{ config('app.name') }}
+
+{{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+        © 2020 <b>Lawbe.co.uk</b> All rights reserved.
+        @endcomponent
+    @endslot
 @endcomponent
+

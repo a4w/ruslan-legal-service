0158118c6d4ebc16b795d255cb84a15ad2fabe68
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-14 09:53:57 +0200

    Removed cors from routemiddleware

--- backend/legal-service-app/app/Http/Kernel.php
+++ backend/legal-service-app/app/Http/Kernel.php
@@ -69,7 +69,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \app\Http\Middleware\CORS::class,
-
     ];
 }

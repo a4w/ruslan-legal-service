0c09ddeaeaaa3c1b39e3258cedc86879fbb2e5db
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-05-16 05:40:07 +0200

    Add post verification page

--- frontend/legal-service-app/src/App.js
+++ frontend/legal-service-app/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import Register from "./Register";
 import PreReleaseHome from "./PreReleaseHome";
 import PreNavBar from "./PreNavBar";
 import PreFooter from "./PreFooter";
-import PostRegistration from "./PostRegisration";
+import PostRegistration from "./PostRegistration";
+import PostVerification from "./PostVerification";
 import {Route, Router, Redirect, Switch} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/style.css'
@@ -21,6 +21,9 @@ function App() {
                 </Route>
                 <Route exact path='/post-registration'>
                     <PostRegistration />
+                </Route>
+                <Route exact path='/post-verification'>
+                    <PostVerification />
                 </Route>
                 <Redirect to='/' />
             </Switch>>
---
+++ frontend/legal-service-app/src/PostVerification.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Wrapper from "./Wrapper";
+
+const PostRegistration = (_) => {
+
+    return (
+        <Wrapper>
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+
+                    <div class="account-content">
+                        <div class="row align-items-center justify-content-center">
+                            <div class="col-md-12 col-lg-6 login-right">
+                                <div className='login-header'>
+                                    <img src="/undraw_remainders.svg" style={{width: "200px", padding: "20px", margin: "auto", display: "block"}} />
+                                    <h5 style={{textAlign: "center"}}>
+                                        <b>Thank you for registering!</b><br />
+                                        You will be notified by email as soon as we launch
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br />
+        </Wrapper>
+    );
+};
+export default PostRegistration;
+

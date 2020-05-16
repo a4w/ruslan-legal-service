c065dd034138e8985b172d834f103e9f24f89545
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-17 01:14:19 +0200

    A failed attempt to generalize imports

--- frontend/legal-service-app/src/App.js
+++ frontend/legal-service-app/src/App.js
@@ -6,7 +6,9 @@ import Login from "./Login";
 import Home from "./Home";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import {Route, BrowserRouter} from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.css";
 
 function App() {
     return (

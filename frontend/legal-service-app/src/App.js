98d8964a1ad39b520cc64cbd8c6453ad9c3fcfad
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-05-17 04:57:03 +0200

    Remove Home and PreRelease Home

--- frontend/legal-service-app/src/App.js
+++ frontend/legal-service-app/src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./App.css";
 import RegisterationForm from "./RegisterationForm";
 import LoginForm from "./LoginForm";
-import Home from "./Home";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import { Route, Router, Switch } from "react-router-dom";
+import {Route, Router, Switch} from "react-router-dom";
 import history from "./History";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/plugins/fontawesome/css/all.css";
@@ -15,17 +14,15 @@ function App() {
     return (
         <Router history={history}>
             <Switch>
-                <Route exact path='/'>
-                    <NavBar />
-                    <Home />
-                    <Footer />
-                </Route>
+                <Route path='/home'>
+                    <h1> Yet to be done </h1>
+                </Route>>
                 <Route path='/login'>
                     <NavBar />
                     <LoginForm />
                     <Footer />
                 </Route>
-                <Route path='/Register'>
+                <Route path='/register'>
                     <NavBar />
                     <RegisterationForm />
                     <Footer />

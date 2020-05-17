d0f1e7b736819bed96cbde118139d42eaca3613e
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-17 04:05:51 +0200

    Added History component

--- frontend/legal-service-app/src/App.js
+++ frontend/legal-service-app/src/App.js
@@ -6,13 +6,14 @@ import Home from "./Home";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import { Route, Router, Switch } from "react-router-dom";
+import history from "./History";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/plugins/fontawesome/css/all.css";
 import "./assets/css/style.css";
 
 function App() {
     return (
-        <Router>
+        <Router history={history}>
             <Switch>
                 <Route exact path='/'>
                     <NavBar />

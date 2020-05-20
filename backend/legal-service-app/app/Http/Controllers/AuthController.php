cc6338021a15f9bd1bcc1cbc44fe38864ccfee5b
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-05-20 18:23:19 +0200

    Using facade to remove IDE error

--- backend/legal-service-app/app/Http/Controllers/AuthController.php
+++ backend/legal-service-app/app/Http/Controllers/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => Auth::factory()->getTTL() * 60
         ]);
     }
 

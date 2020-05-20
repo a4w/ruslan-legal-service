a101ee3ded9a8b100063db0211493c2ab2a87a0b
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-05-21 00:18:50 +0200

    Add JWT refresh token
    This will return a refresh token when logging on which can be used in
    /auth/refresh to generate new access tokens
    This is to mimic the remember me functionality

--- backend/legal-service-app/app/Http/Controllers/AuthController.php
+++ backend/legal-service-app/app/Http/Controllers/AuthController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Account;
 use App\Http\Requests\JSONRequest;
+use Exception;
+use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
@@ -20,7 +23,21 @@ class AuthController extends Controller
         if (!$token = auth()->attempt($credentials)) {
             return $this->respondUnauthorized();
         }
-        return $this->respondWithToken($token);
+        $refresh_token = null;
+        if ($request->get('refresh_token') === true) {
+            // Create refresh_token
+            $key = config('app.key');
+            $payload = array(
+                "iss" => url('/'),
+                "aud" => url('/'),
+                "iat" => now()->unix(),
+                "exp" => now()->addMonths(1)->unix(),
+                "sub" =>  auth()->user()->getAuthIdentifier(),
+                "rea" => 'REFRESH_TOKEN'
+            );
+            $refresh_token = JWT::encode($payload, $key);
+        }
+        return $this->respondWithToken($token, $refresh_token);
     }
 
     public function logout()
@@ -38,7 +55,13 @@ class AuthController extends Controller
         ]);
         $refresh_token = $request->get('refresh_token');
         // Find user with this refresh token
-        $user = Account::where('refresh_token', $refresh_token)->first();
+        $key = config('app.key');
+        try {
+            $jwt = JWT::decode($refresh_token, $key, array('HS256'));
+        } catch (Exception $e) {
+            return $this->respondUnauthorized();
+        }
+        $user = Account::find($jwt->sub);
         if ($user === null) {
             return $this->respondUnauthorized();
         }
@@ -52,13 +75,18 @@ class AuthController extends Controller
         return $this->respondWithToken(Auth::refresh());
     }
 
-    protected function respondWithToken($token)
+    protected function respondWithToken($token, $refresh_token = null)
     {
-        return response()->json([
+        $response = [
             'access_token' => $token,
-            'token_type' => 'Bearer',
-            'expires_in' => Auth::factory()->getTTL() * 60
-        ]);
+            'token_type' => 'Bearer'
+        ];
+        if ($refresh_token !== null) {
+            $response += [
+                'refresh_token' => $refresh_token
+            ];
+        }
+        return response()->json($response);
     }
 
     protected function respondUnauthorized()

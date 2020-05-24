92625574ceeac23e381b6fdce397ada751daba12
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-24 13:15:55 +0200

    Sticky filter panel

--- frontend/legal-service-app/src/LawyerList.js
+++ frontend/legal-service-app/src/LawyerList.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import LawyerCardList from "./LawyerCardList";
 import Select from "react-select";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { StickyContainer, Sticky } from "react-sticky";
 
 function LawyerList() {
     const [selectedValue, setSelected] = useState(null);
@@ -26,10 +29,89 @@ function LawyerList() {
                 OnChangeHandler={OnChangeHandler}
                 selectedValue={selectedValue}
             />
-            <LawyerCardList lawyers={lawyers} />
+            <StickyContainer>
+                <div className="content">
+                    <div className="row">
+                        <div className="col-md-12 col-lg-4 col-xl-3">
+                            <Sticky>
+                                {({ style }) => (
+                                    <div style={style}>
+                                        <LawyerSearchFilter />
+                                    </div>
+                                )}
+                            </Sticky>
+                        </div>
+                        <div className="col-md-12 col-lg-8 col-xl-9">
+                            <LawyerCardList lawyers={lawyers} />
+                        </div>
+                    </div>
+                </div>
+            </StickyContainer>
         </>
     );
 }
+
+const LawyerSearchFilter = () => {
+    const [selectedDate, handleDateChange] = useState();
+    return (
+        <div className="card search-filter">
+            <div className="card-header">
+                <h4 className="card-title mb-0">Search Filter</h4>
+            </div>
+            <div className="card-body">
+                <div className="filter-widget">
+                    <div className="cal-icon">
+                        <DatePicker
+                            className="form-control datetimepicker"
+                            selected={selectedDate}
+                            onChange={(date) => handleDateChange(date)}
+                            maxDate={new Date()}
+                            placeholderText="Select Date"
+                        />
+                    </div>
+                </div>
+                <div className="filter-widget">
+                    <h4>Some Filteration Header</h4>
+                    <div>
+                        <label className="custom_check">
+                            <input type="checkbox" name="select_specialist" />
+                            <span className="checkmark"></span> F1
+                        </label>
+                    </div>
+                    <div>
+                        <label className="custom_check">
+                            <input type="checkbox" name="select_specialist" />
+                            <span className="checkmark"></span> F2
+                        </label>
+                    </div>
+                    <div>
+                        <label className="custom_check">
+                            <input type="checkbox" name="select_specialist" />
+                            <span className="checkmark"></span> F3
+                        </label>
+                    </div>
+                    <div>
+                        <label className="custom_check">
+                            <input type="checkbox" name="select_specialist" />
+                            <span className="checkmark"></span> F4
+                        </label>
+                    </div>
+                    <div>
+                        <label className="custom_check">
+                            <input type="checkbox" name="select_specialist" />
+                            <span className="checkmark"></span> F5
+                        </label>
+                    </div>
+                </div>
+                <div className="btn-search">
+                    <button type="button" className="btn btn-block">
+                        Search
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const LawyerListHeader = ({ OnChangeHandler, selectedValue }) => {
     const options = [

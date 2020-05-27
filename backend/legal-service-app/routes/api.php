836f4aa407fc902260a1b4ab3223b067e60c3211
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-05-27 09:13:57 +0200

    Add lawyer listing route

--- backend/legal-service-app/routes/api.php
+++ backend/legal-service-app/routes/api.php
@@ -22,3 +22,5 @@ Route::prefix('auth')->group(function () {
     Route::post('refreshCurrentToken', 'AuthController@refreshCurrentToken');
     Route::post('refresh', 'AuthController@refresh');
 });
+
+Route::get('/lawyers', 'LawyerController@getLawyersPaginated');

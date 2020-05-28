8cede6489789611416a75a8158969f654eb85331
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-28 18:07:08 +0200

    Initial component

--- frontend/legal-service-app/src/App.js
+++ frontend/legal-service-app/src/App.js
@@ -10,6 +10,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./assets/css/style.css";
 import LawyerList from "./LawyerList";
+import EditPersonal from "./EditPersonal";
 
 function App() {
     return (
@@ -31,6 +32,9 @@ function App() {
                 </Route>
                 <Route exact path="/list">
                     <LawyerList />
+                </Route>
+                <Route wxact path="/edit">
+                    <EditPersonal />
                 </Route>
             </Switch>
         </Router>
---
+++ frontend/legal-service-app/src/EditPersonal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditPersonal = () => {
+    return <div>edit</div>;
+};
+
+export default EditPersonal;

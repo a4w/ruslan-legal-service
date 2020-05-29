a951091799b93f79cd33268336cae17ad78d913f
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-29 12:40:21 +0200

    Extracted Email component

--- frontend/legal-service-app/src/EditEmail.js
+++ frontend/legal-service-app/src/EditEmail.js
@@ -1,26 +1,18 @@
 import React, { useState } from "react";
-import { editBasicInfoValidation } from "./Validations";
+import { editEmailValidations } from "./Validations";
 import useValidation from "./useValidation";
 import ErrorMessageInput from "./ErrorMessageInput";
 import { FaSpinner } from "react-icons/fa";
 
 const EditBasicInfo = () => {
-    const initUser = {
-        name: "",
-        surname: "",
-        email: "",
-        phone: "",
-    };
-
-    const [user, setUser] = useState(initUser);
+    const [user, setUser] = useState({ email: "" });
     const [isSaving, setSaving] = useState(false);
-    const [errors, , runValidation] = useValidation(editBasicInfoValidation);
+    const [errors, , runValidation] = useValidation(editEmailValidations);
 
     const OnChangeHandler = (event) => {
-        const fieldName = event.target.name;
-        const nextUser = { ...user, [fieldName]: event.target.value };
+        const nextUser = { email: event.target.value };
         setUser(nextUser);
-        runValidation(nextUser, fieldName);
+        runValidation(nextUser, "email");
     };
     const OnSubmitHandler = (event) => {
         event.preventDefault();
@@ -37,33 +29,11 @@ const EditBasicInfo = () => {
             <div className="form-row">
                 <div className="col-lg-6 col-md-6 col-sm-12">
                     <ErrorMessageInput
-                        placeholder={"name"}
-                        name={"name"}
-                        value={user.name}
+                        placeholder={"Email"}
+                        name={"email"}
+                        value={user.email}
                         type={"text"}
-                        errors={errors.name}
-                        OnChangeHandler={OnChangeHandler}
-                    />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-12">
-                    <ErrorMessageInput
-                        placeholder={"surname"}
-                        name={"surname"}
-                        value={user.surname}
-                        type={"text"}
-                        errors={errors.surname}
-                        OnChangeHandler={OnChangeHandler}
-                    />
-                </div>
-            </div>
-            <div className="form-row">
-                <div className="col-lg-6 col-md-6 col-sm-12">
-                    <ErrorMessageInput
-                        placeholder={"Telegpone Number"}
-                        name={"phone"}
-                        value={user.phone}
-                        type={"text"}
-                        errors={errors.phone}
+                        errors={errors.email}
                         OnChangeHandler={OnChangeHandler}
                     />
                 </div>

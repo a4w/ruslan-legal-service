964fe1a14c4e865b86207dec06493d387cf84387
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-05-30 07:06:39 +0200

    Select with an Error Message

---
+++ frontend/legal-service-app/src/ErrorMessageSelect.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Select from "react-dropdown-select";
+
+const ErrorMessageSelect = ({
+    errors = [],
+    placeholder,
+    value,
+    name,
+    OnChangeHandler,
+    options,
+}) => {
+    const selectStyle = {
+        borderColor: "#dcdcdc",
+        backgroundColor: "#ffffff",
+        color: "#333",
+        minHeight: "50px",
+    };
+    return (
+        <div className="form-group">
+            <Select
+                name={name}
+                className="floating"
+                value={value}
+                style={selectStyle}
+                placeholder={placeholder}
+                options={options}
+                onChange={OnChangeHandler}
+            />
+            {errors.length > 0 && (
+                <label className="text-danger ml-2 font-weight-light text-xs">
+                    {errors[0]}
+                </label>
+            )}
+        </div>
+    );
+};
+
+export default ErrorMessageSelect;

58efaaff37ef6f496052fc1478f3a0f0aa7ddf73
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-05-31 03:14:02 +0200

    Add default momentjs formats

--- frontend/legal-service-app/src/Config.js
+++ frontend/legal-service-app/src/Config.js
@@ -1,5 +1,7 @@
 const Config = {
     // api_url: "http://testingapi.lawbe.co.uk",
     api_url: "http://127.0.0.1:8000/api",
+    momentsjs_default_datetime_format: "Y-MM-DD HH:mm:ss",
+    momentsjs_default_date_format: "Y-MM-DD"
 };
 export default Config;

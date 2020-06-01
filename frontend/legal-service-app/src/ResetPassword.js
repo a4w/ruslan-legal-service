032dfca8e23e145d60e2a9ae001ddeb148bd4488
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-01 06:23:08 +0200

    Reset password form

--- frontend/legal-service-app/src/ResetPassword.js
+++ frontend/legal-service-app/src/ResetPassword.js
@@ -1,5 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import ErrorMessageInput from "./ErrorMessageInput";
 const ResetPassword = () => {
-    return <div>reset</div>;
+    const initUser = {
+        newPassword: "",
+        passwordConfirm: "",
+    };
+
+    const [user, setUser] = useState(initUser);
+
+    const OnChangeHandler = ({ target: { name, value } }) => {
+        const nextUser = { ...user, [name]: value };
+        setUser(nextUser);
+    };
+
+    const OnSubmitHandler = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={OnSubmitHandler}>
+                <div className="form-row">
+                    <div className="col-lg-5 col-md-5 col-sm-12">
+                        <ErrorMessageInput
+                            placeholder={"New Password"}
+                            name={"newPassword"}
+                            value={user.newPassword}
+                            type={"password"}
+                            OnChangeHandler={OnChangeHandler}
+                        />
+                    </div>
+                    <div className="col-lg-5 col-md-5 col-sm-12">
+                        <ErrorMessageInput
+                            placeholder={"Re-Enter Password"}
+                            name={"passwordConfirm"}
+                            value={user.passwordConfirm}
+                            type={"password"}
+                            OnChangeHandler={OnChangeHandler}
+                        />
+                    </div>
+                    <div className="col-lg-2 col-md-2 col-sm-12 float-right">
+                        <button
+                            className="btn btn-primary btn-block btn-lg login-btn "
+                            type="submit"
+                        >
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 };
+
 export default ResetPassword;

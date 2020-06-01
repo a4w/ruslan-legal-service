aca524153362572ec291cca6d469fd901ac3c430
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-01 08:13:40 +0200

    Added Search Form

--- frontend/legal-service-app/src/Blogs.js
+++ frontend/legal-service-app/src/Blogs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BlogList from "./BlogList";
 
 const Blogs = () => {
@@ -25,10 +25,42 @@ const Blogs = () => {
                 <BlogList blogs={blogs} />
             </div>
             <div className="col-lg-4 col-md-12 sidebar-right theiaStickySidebar">
-                sideBar
+                <Search />
             </div>
         </div>
     );
 };
 
+const Search = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const OnChangeHandler = ({ target: { value } }) => {
+        setSearchInput(value);
+    };
+    const OnSubmitHandler = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className="card search-widget">
+            <div className="card-body">
+                <form className="search-form" onSubmit={OnSubmitHandler}>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="form-control"
+                            value={searchInput}
+                            name="searchInput"
+                            onChange={OnChangeHandler}
+                        />
+                        <div className="input-group-append">
+                            <button type="submit" className="btn btn-primary">
+                                <i className="fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
 export default Blogs;

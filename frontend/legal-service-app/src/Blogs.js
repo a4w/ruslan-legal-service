e0b7c388579872a2f56b0ceac47736b5574cc3aa
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-01 09:02:29 +0200

    Added Tags List

--- frontend/legal-service-app/src/Blogs.js
+++ frontend/legal-service-app/src/Blogs.js
@@ -32,6 +32,7 @@ const Blogs = () => {
                 <Search setBlogs={setBlogs} />
                 <LatestBlogs latest={blogs} />
                 <Catagories />
+                <TagsList />
             </div>
         </div>
     );
@@ -102,8 +103,8 @@ const LatestBlogList = ({ latest }) => {
     return (
         <ul className="latest-posts">
             {latest.map((blog) => (
-                <li>
-                    <LatestBlogCard key={blog.id} lawer={blog} />
+                <li key={blog.id}>
+                    <LatestBlogCard lawer={blog} />
                 </li>
             ))}
         </ul>
@@ -112,16 +113,49 @@ const LatestBlogList = ({ latest }) => {
 const Catagories = () => {
     const cats = ["cat 1", "cat 2", "cat 3"];
     return (
-        <div class="card category-widget">
-            <div class="card-header">
-                <h4 class="card-title">Blog Categories</h4>
+        <div className="card category-widget">
+            <div className="card-header">
+                <h4 className="card-title">Blog Categories</h4>
             </div>
-            <div class="card-body">
-                <ul class="categories">
+            <div className="card-body">
+                <ul className="categories">
                     {cats.map((cat) => (
-                        <li>
+                        <li key={cat}>
                             <a href="//">
                                 {cat} <span>subject num</span>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const TagsList = () => {
+    const tags = [
+        "Tag 1",
+        "Tag 2",
+        "Tag 3",
+        "Tag 4",
+        "Tag 5",
+        "Tag 6",
+        "Tag 7",
+        "Tag 8",
+        "Tag 9",
+        "Tag n",
+    ];
+    return (
+        <div className="card tags-widget">
+            <div className="card-header">
+                <h4 className="card-title">Tags</h4>
+            </div>
+            <div className="card-body">
+                <ul className="tags">
+                    {tags.map((tag) => (
+                        <li key={tag}>
+                            <a href="//" className="tag">
+                                {tag}
                             </a>
                         </li>
                     ))}

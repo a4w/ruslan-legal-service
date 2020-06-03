8f9f4cb35076153a2e60c37a10501d6dfd25b346
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-03 12:02:32 +0200

    Added all tabs

--- frontend/legal-service-app/src/LawyerDashboardTabs.js
+++ frontend/legal-service-app/src/LawyerDashboardTabs.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import Tab from "react-bootstrap/Tab";
 
 const LawyerDashboardTabs = () => {
     return (
-        <div>
-           
-        </div>
+        <Tab.Content>
+            <Tab.Pane eventKey="dashboard">test 1</Tab.Pane>
+            <Tab.Pane eventKey="appointments">test 2</Tab.Pane>
+            <Tab.Pane eventKey="clients">test 3</Tab.Pane>
+            <Tab.Pane eventKey="schedule">test 4</Tab.Pane>
+            <Tab.Pane eventKey="invoices">test 5</Tab.Pane>
+            <Tab.Pane eventKey="reviews">test 6</Tab.Pane>
+            <Tab.Pane eventKey="messages">test 7</Tab.Pane>
+            <Tab.Pane eventKey="settings">test 8</Tab.Pane>
+            <Tab.Pane eventKey="social-media">test 9</Tab.Pane>
+            <Tab.Pane eventKey="change-password">test 10</Tab.Pane>
+            <Tab.Pane eventKey="logout">test 11</Tab.Pane>
+        </Tab.Content>
     );
 };
 export default LawyerDashboardTabs;

ece765a0a77f7baa5d2cdbd2d958ceed7cc5be62
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-03 10:36:37 +0200

    Sidebar with tabs for links

--- frontend/legal-service-app/src/LawyerDashboardSidebar.js
+++ frontend/legal-service-app/src/LawyerDashboardSidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "react-bootstrap/Nav";
 
 const LawyerDashboardSidebar = () => {
     return (
@@ -6,94 +7,91 @@ const LawyerDashboardSidebar = () => {
             <div className="widget-profile pro-widget-content">
                 <div className="profile-info-widget">
                     <a href="//" className="booking-lawyer-img">
-                        <img
-                            src="assets/img/doctors/lawyer-thumb-02.jpg"
-                            alt="User"
-                        />
+                        Lawyer's Photo
                     </a>
                     <div className="profile-det-info">
-                        <h3>Dr. Darren Elder</h3>
+                        <h3>Lawyer's Name</h3>
 
                         <div className="client-details">
-                            <h5 className="mb-0">
-                                BDS, MDS - Oral & Maxillofacial Surgery
-                            </h5>
+                            <h5 className="mb-0">Lawyer's Area of expertice</h5>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="dashboard-widget">
-                <nav className="dashboard-menu">
-                    <ul>
-                        <li className="active">
-                            <a href="lawyer-dashboard.html">
-                                <i className="fas fa-columns"></i>
-                                <span>Dashboard</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="appointments.html">
-                                <i className="fas fa-calendar-check"></i>
-                                <span>Appointments</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="my-patients.html">
-                                <i className="fas fa-user-injured"></i>
-                                <span>My Patients</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="schedule-timings.html">
-                                <i className="fas fa-hourglass-start"></i>
-                                <span>Schedule Timings</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="invoices.html">
-                                <i className="fas fa-file-invoice"></i>
-                                <span>Invoices</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="reviews.html">
-                                <i className="fas fa-star"></i>
-                                <span>Reviews</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="chat-lawyer.html">
-                                <i className="fas fa-comments"></i>
-                                <span>Message</span>
-                                <small className="unread-msg">23</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="lawyer-profile-settings.html">
-                                <i className="fas fa-user-cog"></i>
-                                <span>Profile Settings</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="social-media.html">
-                                <i className="fas fa-share-alt"></i>
-                                <span>Social Media</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="lawyer-change-password.html">
-                                <i className="fas fa-lock"></i>
-                                <span>Change Password</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="index.html">
-                                <i className="fas fa-sign-out-alt"></i>
-                                <span>Logout</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+                <Nav className="dashboard-menu">
+                    <div className="dashboard-menu">
+                        <ul>
+                            <li>
+                                <Nav.Link eventKey="dashboard">
+                                    <i className="fas fa-columns"></i>
+                                    <span> Dashboard</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="appointments">
+                                    <i className="fas fa-calendar-check"></i>
+                                    <span>Appointments</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="clients">
+                                    <i className="fas fa-user-injured"></i>
+                                    <span>My Clients</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="schedule">
+                                    <i className="fas fa-hourglass-start"></i>
+                                    <span>Schedule Timings</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="invoices">
+                                    <i className="fas fa-file-invoice"></i>
+                                    <span>Invoices</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="reviews">
+                                    <i className="fas fa-star"></i>
+                                    <span>Reviews</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="messages">
+                                    <i className="fas fa-comments"></i>
+                                    <span>Messages</span>
+                                    <small className="unread-msg">23</small>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="settings">
+                                    <i className="fas fa-user-cog"></i>
+                                    <span>Profile Settings</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="social-media">
+                                    <i className="fas fa-share-alt"></i>
+                                    <span>Social Media</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="change-password">
+                                    <i className="fas fa-lock"></i>
+                                    <span>Change Password</span>
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link eventKey="logout">
+                                    <i className="fas fa-sign-out-alt"></i>
+                                    <span>Logout</span>
+                                </Nav.Link>
+                            </li>
+                        </ul>
+                    </div>
+                </Nav>
             </div>
         </div>
     );

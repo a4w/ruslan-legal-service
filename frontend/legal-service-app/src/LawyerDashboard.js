65be8b7c05862b2f7b66a4273723806ca8d5e409
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-03 10:43:10 +0200

    Added Lawyer dashboard tab component

--- frontend/legal-service-app/src/LawyerDashboard.js
+++ frontend/legal-service-app/src/LawyerDashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import LawyerDashboardSidebar from "./LawyerDashboardSidebar";
 import StickyBox from "react-sticky-box";
 import Tab from "react-bootstrap/Tab";
+import LawyerDashboardTabs from "./LawyerDashboardTabs";
 
 const LawyerDashboard = () => {
     return (
@@ -9,9 +10,12 @@ const LawyerDashboard = () => {
             {" "}
             <Tab.Container id="dashboard" defaultActiveKey="first">
                 <div className="col-md-5 col-lg-4 col-xl-3 theiaStickySidebar">
-                <StickyBox offsetTop={20} offsetBottom={20}>
-                    <LawyerDashboardSidebar />
-                </StickyBox>
+                    <StickyBox offsetTop={20} offsetBottom={20}>
+                        <LawyerDashboardSidebar />
+                    </StickyBox>
+                </div>
+                <div class="col-md-7 col-lg-8 col-xl-9">
+                    <LawyerDashboardTabs />
                 </div>
             </Tab.Container>
         </div>

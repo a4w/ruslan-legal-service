23dcc87ea8962c34bad3207c5406965151a9e896
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-03 10:38:16 +0200

    Added tab container to dashboard

--- frontend/legal-service-app/src/LawyerDashboard.js
+++ frontend/legal-service-app/src/LawyerDashboard.js
@@ -1,13 +1,16 @@
 import React from "react";
 import LawyerDashboardSidebar from "./LawyerDashboardSidebar";
+import Tab from "react-bootstrap/Tab";
 
 const LawyerDashboard = () => {
     return (
         <div>
             {" "}
-            <div className="col-md-5 col-lg-4 col-xl-3 theiaStickySidebar">
-                <LawyerDashboardSidebar />
-            </div>
+            <Tab.Container id="dashboard" defaultActiveKey="first">
+                <div className="col-md-5 col-lg-4 col-xl-3 theiaStickySidebar">
+                    <LawyerDashboardSidebar />
+                </div>
+            </Tab.Container>
         </div>
     );
 };

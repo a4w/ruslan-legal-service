ef570a16043c5dce01d5e5cd2e58aa5b45028b88
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-03 12:46:01 +0200

    Added appointments to tabs

--- frontend/legal-service-app/src/LawyerDashboardTabs.js
+++ frontend/legal-service-app/src/LawyerDashboardTabs.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Tab from "react-bootstrap/Tab";
 import EditPassword from "./EditPassword";
-import EditPersonal, {Content} from "./EditPersonal";
+import EditPersonal, { Content } from "./EditPersonal";
+import LawyerAppointments from "./LawyerAppointments";
 
 const LawyerDashboardTabs = () => {
     return (
         <Tab.Content>
             <Tab.Pane eventKey="dashboard">test 1</Tab.Pane>
-            <Tab.Pane eventKey="appointments">test 2</Tab.Pane>
+            <Tab.Pane eventKey="appointments">
+                <LawyerAppointments />
+            </Tab.Pane>
             <Tab.Pane eventKey="clients">test 3</Tab.Pane>
             <Tab.Pane eventKey="schedule">test 4</Tab.Pane>
             <Tab.Pane eventKey="invoices">test 5</Tab.Pane>

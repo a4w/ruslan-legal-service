1b6e2a88657bcd3f497a71447994ce75e534e011
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-03 10:40:38 +0200

    Made sidebar sticky

--- frontend/legal-service-app/src/LawyerDashboard.js
+++ frontend/legal-service-app/src/LawyerDashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LawyerDashboardSidebar from "./LawyerDashboardSidebar";
+import StickyBox from "react-sticky-box";
 import Tab from "react-bootstrap/Tab";
 
 const LawyerDashboard = () => {
@@ -8,7 +9,9 @@ const LawyerDashboard = () => {
             {" "}
             <Tab.Container id="dashboard" defaultActiveKey="first">
                 <div className="col-md-5 col-lg-4 col-xl-3 theiaStickySidebar">
+                <StickyBox offsetTop={20} offsetBottom={20}>
                     <LawyerDashboardSidebar />
+                </StickyBox>
                 </div>
             </Tab.Container>
         </div>

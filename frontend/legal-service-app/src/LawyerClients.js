b928ab303539a7176ab0322debf2d49701f5723f
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-05 18:17:46 +0200

    Client Card display

--- frontend/legal-service-app/src/LawyerClients.js
+++ frontend/legal-service-app/src/LawyerClients.js
@@ -5,7 +5,48 @@ const LawyerClients = () => {
 };
 
 const ClientCard = ({ client }) => {
-    return <div>card</div>;
+    return (
+        <div className="col-md-6 col-lg-4 col-xl-3">
+            <div className="card widget-profile pat-widget-profile">
+                <div className="card-body">
+                    <div className="pro-widget-content">
+                        <div className="profile-info-widget">
+                            <a
+                                href="client-profile.html"
+                                className="booking-lawyer-img"
+                            >
+                                Profile Pic
+                            </a>
+                            <div className="profile-det-info">
+                                <h3>
+                                    <a href="client-profile.html">
+                                        Client's name
+                                    </a>
+                                </h3>
+
+                                <div className="client-details">
+                                    <h5 className="mb-0">
+                                        <i className="fas fa-map-marker-alt"></i>{" "}
+                                        City, Country
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="client-info">
+                        <ul>
+                            <li>
+                                Phone <span>+x xxx xxx xxxx</span>
+                            </li>
+                            <li>
+                                data <span>etc..</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LawyerClients;

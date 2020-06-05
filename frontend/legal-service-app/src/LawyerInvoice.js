7fdec77a96e2f7c83326cbf13c6ca1a95f9eb2c8
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-05 19:03:52 +0200

    Invoice table styling

--- frontend/legal-service-app/src/LawyerInvoice.js
+++ frontend/legal-service-app/src/LawyerInvoice.js
@@ -1,23 +1,29 @@
 import React from "react";
 
 const LawyerInvoice = () => {
-    return <InvoiceTable />;
+    return <InvoiceTable></InvoiceTable>;
 };
 
-const InvoiceTable = () => {
+const InvoiceTable = (props) => {
     return (
-        <table className="table table-hover table-center mb-0">
-            <thead>
-                <tr>
-                    <th>Invoice No</th>
-                    <th>Patient</th>
-                    <th>Amount</th>
-                    <th>Paid On</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody></tbody>
-        </table>
+        <div className="card card-table">
+            <div className="card-body">
+                <div className="table-responsive">
+                    <table className="table table-hover table-center mb-0">
+                        <thead>
+                            <tr>
+                                <th>Invoice No</th>
+                                <th>Client</th>
+                                <th>Amount</th>
+                                <th>Paid On</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>{props.children}</tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     );
 };
 export default LawyerInvoice;

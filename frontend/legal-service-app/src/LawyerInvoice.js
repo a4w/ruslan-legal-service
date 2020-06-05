73720b6fb0026be7007c78619ad7f56df7e0dd94
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-05 19:07:25 +0200

    Invoice table row component

--- frontend/legal-service-app/src/LawyerInvoice.js
+++ frontend/legal-service-app/src/LawyerInvoice.js
@@ -1,9 +1,51 @@
 import React from "react";
 
 const LawyerInvoice = () => {
-    return <InvoiceTable></InvoiceTable>;
+    return (
+        <InvoiceTable>
+            <InvoiceTableRow />
+        </InvoiceTable>
+    );
 };
-
+const InvoiceTableRow = () => {
+    return (
+        <tr>
+            <td>
+                <a href="invoice-view.html">ID</a>
+            </td>
+            <td>
+                <h2 className="table-avatar">
+                    <a
+                        href="patient-profile.html"
+                        className="avatar avatar-sm mr-2"
+                    >
+                        {/* <img
+                            className="avatar-img rounded-circle"
+                            src="assets/img/patients/patient.jpg"
+                            alt="User"
+                        /> */}
+                        IMG
+                    </a>
+                    <a href="patient-profile.html">
+                        Client's name <span> ID </span>
+                    </a>
+                </h2>
+            </td>
+            <td>Amount</td>
+            <td>Date Payed?</td>
+            <td className="text-right">
+                <div className="table-action">
+                    <button className="btn btn-sm bg-info-light m-1">
+                        <i className="far fa-eye"></i> View
+                    </button>
+                    <button className="btn btn-sm bg-primary-light m-1">
+                        <i className="fas fa-print"></i> Print
+                    </button>
+                </div>
+            </td>
+        </tr>
+    );
+};
 const InvoiceTable = (props) => {
     return (
         <div className="card card-table">

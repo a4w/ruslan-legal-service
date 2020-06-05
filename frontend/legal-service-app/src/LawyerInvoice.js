7eea62ef6f9e4661d2c85d73e7af2b800463d36a
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-05 19:00:40 +0200

    Invoice table header

--- frontend/legal-service-app/src/LawyerInvoice.js
+++ frontend/legal-service-app/src/LawyerInvoice.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const LawyerInvoice = () => {
-    return <div>LawyerInvoice</div>;
+    return <InvoiceTable />;
 };
 
+const InvoiceTable = () => {
+    return (
+        <table className="table table-hover table-center mb-0">
+            <thead>
+                <tr>
+                    <th>Invoice No</th>
+                    <th>Patient</th>
+                    <th>Amount</th>
+                    <th>Paid On</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    );
+};
 export default LawyerInvoice;

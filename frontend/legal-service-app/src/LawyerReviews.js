dac4a39a72511cb66e025e2d18465e5636a1e669
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-06 08:42:04 +0200

    Added review list dtylings

--- frontend/legal-service-app/src/LawyerReviews.js
+++ frontend/legal-service-app/src/LawyerReviews.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const LawyerReviews = () => {
-    return <div className="row">reviews</div>;
+    return <ReviewList />;
+};
+
+const ReviewList = () => {
+    return (
+        <div class="doc-review review-listing">
+            <ul class="comments-list">reviews</ul>
+        </div>
+    );
 };
 export default LawyerReviews;

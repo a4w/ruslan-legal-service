50734c9e8b7dc50aec394ee3a20c415664061e94
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-06 10:50:40 +0200

    Review with stars

--- frontend/legal-service-app/src/LawyerReviews.js
+++ frontend/legal-service-app/src/LawyerReviews.js
@@ -1,4 +1,5 @@
 import React from "react";
+import StarRatings from "react-star-ratings";
 
 const LawyerReviews = () => {
     return <ReviewList />;
@@ -6,8 +7,44 @@ const LawyerReviews = () => {
 
 const ReviewList = () => {
     return (
-        <div class="doc-review review-listing">
-            <ul class="comments-list">reviews</ul>
+        <div className="lawyer-review review-listing">
+            <ul className="comments-list">
+                <li>
+                    <Comment />
+                </li>
+            </ul>
+        </div>
+    );
+};
+const Comment = () => {
+    return (
+        <div className="comment">
+            {/* <img
+                className="avatar rounded-circle"
+                alt="User"
+                src="assets/img/patients/patient.jpg"
+            /> */}
+            Client's Pic
+            <div className="comment-body" style={{ width: "100%" }}>
+                <div className="meta-data">
+                    <span className="comment-author">Clients name</span>
+                    <span className="comment-date">Review Date</span>
+                    <div className="review-count rating">
+                        <StarRatings
+                            rating={4}
+                            starRatedColor="gold"
+                            starDimension="20px"
+                            starSpacing="0px"
+                            numberOfStars={5}
+                            name="rating"
+                        />
+                    </div>
+                </div>
+                <p className="recommended">
+                    <i className="far fa-thumbs-up"></i> I recommend this Lawyer
+                </p>
+                <p className="comment-content">Coments...</p>
+            </div>
         </div>
     );
 };

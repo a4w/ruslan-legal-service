1c49c9de548ec7e68cf671967e74c7b0520d93a0
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-06 04:31:12 +0200

    Add update email endpoint

--- backend/legal-service-app/routes/api.php
+++ backend/legal-service-app/routes/api.php
@@ -30,3 +30,4 @@ Route::post('/account/reset-password-request', 'AccountController@resetPasswordR
 Route::post('/account/reset-password/{token}', 'AccountController@resetPassword');
 
 Route::post('/account/personal-info', 'AccountController@savePersonalInfo')->middleware('auth:api');
+Route::post('/account/update-email', 'AccountController@updateEmail')->middleware('auth:api');

bb99e39c5acad89da05a129c796bb77aa45f2e60
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-07 17:27:29 +0200

    Restyled tabs in edit personal

--- frontend/legal-service-app/src/EditPersonal.js
+++ frontend/legal-service-app/src/EditPersonal.js
@@ -3,45 +3,46 @@ import Tab from "react-bootstrap/Tab";
 import Nav from "react-bootstrap/Nav";
 import EditBasicInfo from "./EditBasicInfo";
 import EditLawyerInfo from "./EditLawyerInfo";
+import EditEmail from "./EditEmail";
+import EditAddress from "./EditAddress";
 
 const EditPersonal = () => {
     return (
-        <Tab.Container id="personal-info" defaultActiveKey="peronal-info">
-            <div className="row">
-                <div className="col-lg-3 col-md-2 col-sm-12">
-                    <Nav variant="pills" className="flex-column">
-                        <Nav.Item>
-                            <Nav.Link eventKey="peronal-info">Personal Info</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="lawyer-info">Lawyer Info</Nav.Link>
-                        </Nav.Item>
+        <div className="card">
+            <div className="card-body pt-0">
+                <Tab.Container id="personal-info" defaultActiveKey="basic-info">
+                    <Nav className="user-tabs mb-4">
+                        <ul
+                            className="nav nav-tabs nav-tabs-bottom nav-justified"
+                            style={{ width: "100%" }}
+                        >
+                            <li className="nav-item">
+                                <Nav.Link eventKey="basic-info">Basic</Nav.Link>
+                            </li>
+                            <li className="nav-item">
+                                <Nav.Link eventKey="lawyer-info">
+                                    Lawyer
+                                </Nav.Link>
+                            </li>
+                        </ul>
                     </Nav>
-                </div>
-                <div className="col-lg-9 col-md-10 col-sm-12">
+
                     <Tab.Content>
-                        <Tab.Pane eventKey="peronal-info">
-                            <Content>
-                                <EditBasicInfo />
-                            </Content>
+                        <Tab.Pane eventKey="basic-info">
+                            <EditBasicInfo />
+                            <hr></hr>
+                            <EditEmail />
+                            <hr></hr>
+                            <EditAddress />{" "}
                         </Tab.Pane>
                         <Tab.Pane eventKey="lawyer-info">
-                            <Content>
-                                <EditLawyerInfo />
-                            </Content>
+                            <EditLawyerInfo />
                         </Tab.Pane>
                     </Tab.Content>
-                </div>
+                </Tab.Container>
             </div>
-        </Tab.Container>
-    );
-};
-const Content = (props) => {
-    return (
-        <div className="align-items-center justify-content-center m-1">
-            <div className="tab-form">{props.children}</div>
         </div>
     );
 };
+
 export default EditPersonal;
-export {Content};

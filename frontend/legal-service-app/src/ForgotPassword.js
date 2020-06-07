2271f95d3459245442b2215df0d0195531c969ba
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-06-07 20:13:26 +0200

    Made bg color white and added container devs

--- frontend/legal-service-app/src/ForgotPassword.js
+++ frontend/legal-service-app/src/ForgotPassword.js
@@ -21,41 +21,52 @@ const ForgotPassword = () => {
     };
 
     return (
-        <div className="account-content">
-            <div className="row align-items-center justify-content-center">
-                <div className="col-md-7 col-lg-6 login-left">
-                    <img
-                        src="/undraw_my_password_d6kg.svg"
-                        className="img-fluid"
-                        alt="Fogot Password"
-                    />
-                </div>
-                <div className="col-md-12 col-lg-6 login-right">
-                    <div className="login-header">
-                        <h3>Forgot Password?</h3>
-                        <p className="small text-muted">
-                            Enter your email to get a password reset link
-                        </p>
-                    </div>
+        <div class="content" style={{ backgroundColor: "#ffffff" }}>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <div className="account-content">
+                            <div className="row align-items-center justify-content-center">
+                                <div className="col-md-7 col-lg-6 login-left">
+                                    <img
+                                        src="/undraw_my_password_d6kg.svg"
+                                        className="img-fluid"
+                                        alt="Fogot Password"
+                                    />
+                                </div>
+                                <div className="col-md-12 col-lg-6 login-right">
+                                    <div className="login-header">
+                                        <h3>Forgot Password?</h3>
+                                        <p className="small text-muted">
+                                            Enter your email to get a password
+                                            reset link
+                                        </p>
+                                    </div>
 
-                    <form onSubmit={OnSubmitHandler}>
-                        <div className="form-group form-focus">
-                            <ErrorMessageInput
-                                placeholder={"Email"}
-                                name={"email"}
-                                value={email}
-                                type={"text"}
-                                errors={errors.email}
-                                OnChangeHandler={OnChangeHandler}
-                            />
+                                    <form onSubmit={OnSubmitHandler}>
+                                        <div className="form-group form-focus">
+                                            <ErrorMessageInput
+                                                placeholder={"Email"}
+                                                name={"email"}
+                                                value={email}
+                                                type={"text"}
+                                                errors={errors.email}
+                                                OnChangeHandler={
+                                                    OnChangeHandler
+                                                }
+                                            />
+                                        </div>
+                                        <button
+                                            className="btn btn-primary btn-block btn-lg login-btn"
+                                            type="submit"
+                                        >
+                                            Reset Password
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
-                        <button
-                            className="btn btn-primary btn-block btn-lg login-btn"
-                            type="submit"
-                        >
-                            Reset Password
-                        </button>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>

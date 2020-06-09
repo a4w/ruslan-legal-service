e18d426f2ddf73f9737322dd9dfe05e2c7914409
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-09 09:02:56 +0200

    Handle empty comments

--- backend/legal-service-app/app/Http/Controllers/RatingsController.php
+++ backend/legal-service-app/app/Http/Controllers/RatingsController.php
@@ -14,18 +14,21 @@ class RatingsController extends Controller
     {
         // Validation
         $request->validate([
-            'rating' => ['required', 'numeric', 'min:0', 'max:5']
+            'rating' => ['required', 'numeric', 'min:0', 'max:5'],
+            'comment' => []
         ]);
         // Check that the user has this appointment
         /** @var Account **/
         $user = Auth::user()->client;
         $appointment = Appointment::find($appointment_id);
+        $data = $request->only(['rating', 'comment']);
+        $data['comment'] = $data['comment'] === null ? '' : trim($data['comment']);
         if ($user == $appointment->client) {
             // Add rating
             if ($appointment->rating === null) {
-                $appointment->rating()->save(Rating::make($request->only('rating', 'comment')));
+                $appointment->rating()->save(Rating::make($data));
             } else {
-                $appointment->rating->update($request->only('rating', 'comment'));
+                $appointment->rating->update($data);
             }
 
             return ["error" => false];

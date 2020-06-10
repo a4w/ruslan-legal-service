df9a2616fda742d9fee7148bc746eee9a36efb26
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-10 03:40:00 +0200

    Initial fetch chat method

--- backend/legal-service-app/app/Http/Controllers/ChatController.php
+++ backend/legal-service-app/app/Http/Controllers/ChatController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Account;
+use App\Http\Requests\JSONRequest;
 
 class ChatController extends Controller
 {
-    //
+    public function getChat($u1, $u2, JSONRequest $request)
+    {
+        $user_1 = Account::find($u1);
+        $user_2 = Account::find($u2);
+    }
 }

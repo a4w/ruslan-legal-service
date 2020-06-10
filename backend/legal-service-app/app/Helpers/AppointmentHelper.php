ab1e23d96e0f60b2f8740fa6adcbbf133773d340
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-10 02:59:05 +0200

    Add clockToMinutes function to AppointmentHelper

--- backend/legal-service-app/app/Helpers/AppointmentHelper.php
+++ backend/legal-service-app/app/Helpers/AppointmentHelper.php
@@ -2,16 +2,31 @@
 
 namespace App\Helpers;
 
+use Exception;
+use Illuminate\Support\Str;
+
 class AppointmentHelper
 {
     public static $DATETIME_FORMAT = 'Y-m-d H:i:s';
     public static $DATE_FORMAT = 'Y-m-d';
     public static $TIME_FORMAT = 'H:i';
+    public const MINUTES_PER_DAY = 60 * 24;
 
     public static function minutesToClock($minutes)
     {
         $hour = $minutes / 60;
         $minute = $minutes % 60;
         return sprintf('%02d', $hour) . ':' . sprintf('%02d', $minute);
+    }
+
+    public static function clockToMinutes(String $clock): Int
+    {
+        $parts = explode(':', $clock);
+        $hour = (int) $parts[0];
+        $minutes = (int) $parts[1];
+        if ($hour < 0 || $hour > 23 || $minutes < 0 || $minutes > 59) {
+            throw new Exception("Clock is not properly formated");
+        }
+        return 60 * $hour + $minutes;
     }
 }

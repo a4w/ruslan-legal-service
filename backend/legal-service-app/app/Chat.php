03b248d06eaa4f3da33bc9c3a554b99d44810f42
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-10 08:15:17 +0200

    Add chat to messages relationship

--- backend/legal-service-app/app/Chat.php
+++ backend/legal-service-app/app/Chat.php
@@ -10,4 +10,9 @@ class Chat extends Model
     {
         return $this->belongsToMany(Account::class, 'chat_participents', 'chat_id', 'user_id');
     }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }

00851ae8fcf2fa80814825a3eeafb12534ea5887
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-10 07:53:52 +0200

    Implement get chat method

--- backend/legal-service-app/app/Http/Controllers/ChatController.php
+++ backend/legal-service-app/app/Http/Controllers/ChatController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Account;
+use App\Chat;
+use App\ChatParticipent;
 use App\Http\Requests\JSONRequest;
 
 class ChatController extends Controller
@@ -11,5 +13,23 @@ class ChatController extends Controller
     {
         $user_1 = Account::find($u1);
         $user_2 = Account::find($u2);
+        // TODO: Check if sender is one of those
+        // dump(Chat::whereIn('participents', [$user_1, $user_2])->get());
+        $chat = Chat::whereHas('participents', function ($query) use ($user_1, $user_2) {
+            $query->whereIn('user_id', [$user_1->id, $user_2->id]);
+        }, '=', 2)->first();
+        if ($chat === null) {
+            // Create chat
+            $chat = Chat::create();
+            $p1 = ChatParticipent::make();
+            $p1->account()->associate($user_1);
+            $p2 = ChatParticipent::make();
+            $p2->account()->associate($user_2);
+            $chat->participents()->save($p1);
+            $chat->participents()->save($p2);
+            return $chat->id;
+        } else {
+            return $chat->id;
+        }
     }
 }

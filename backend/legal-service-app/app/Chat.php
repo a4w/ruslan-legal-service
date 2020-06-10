c30e765817be62c5ddae36ee263c9f0d711e015f
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-10 07:53:12 +0200

    Add participents relationship to Chat model

--- backend/legal-service-app/app/Chat.php
+++ backend/legal-service-app/app/Chat.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    public function participents()
+    {
+        return $this->hasMany(ChatParticipent::class);
+    }
 }

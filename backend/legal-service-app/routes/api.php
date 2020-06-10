1e0bac119b61ff58dfde2b751744be7a75d43fe1
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-10 03:39:42 +0200

    Change naming of route variables

--- backend/legal-service-app/routes/api.php
+++ backend/legal-service-app/routes/api.php
@@ -33,4 +33,4 @@ Route::post('/account/personal-info', 'AccountController@savePersonalInfo')->mid
 Route::post('/account/update-email', 'AccountController@updateEmail')->middleware('auth:api');
 Route::post('/account/update-password', 'AccountController@updatePassword')->middleware('auth:api');
 
-Route::post('/chat/{user}/{user}', 'ChatController@getChat');
+Route::post('/chat/{account1_id}/{account2_id}', 'ChatController@getChat');

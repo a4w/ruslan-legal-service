558eb46e6ee23648822942a10b986ffe7194a336
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-10 07:56:15 +0200

    Check if authenticated to fetch the chat

--- backend/legal-service-app/app/Http/Controllers/ChatController.php
+++ backend/legal-service-app/app/Http/Controllers/ChatController.php
@@ -6,6 +6,7 @@ use App\Account;
 use App\Chat;
 use App\ChatParticipent;
 use App\Http\Requests\JSONRequest;
+use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
@@ -13,8 +14,13 @@ class ChatController extends Controller
     {
         $user_1 = Account::find($u1);
         $user_2 = Account::find($u2);
-        // TODO: Check if sender is one of those
-        // dump(Chat::whereIn('participents', [$user_1, $user_2])->get());
+        $sender = Auth::user();
+        if ($sender != $user_1 && $sender != $user_2) {
+            return [
+                'error' => true,
+                'message' => 'unauthorized'
+            ];
+        }
         $chat = Chat::whereHas('participents', function ($query) use ($user_1, $user_2) {
             $query->whereIn('user_id', [$user_1->id, $user_2->id]);
         }, '=', 2)->first();

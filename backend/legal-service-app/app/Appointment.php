4441b0da2415e3b65dbd8dd935793520bd7aa606
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-11 10:11:12 +0200

    Add fillable fields of appointment

--- backend/legal-service-app/app/Appointment.php
+++ backend/legal-service-app/app/Appointment.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
 
+    protected $fillable = [
+        'appointment_time', 'status', 'price', 'duration'
+    ];
+
     protected $casts = [
         'appointment_time' => 'datetime',
     ];

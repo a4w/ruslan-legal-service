e24f0be73c240cdfef123c4a78434edd7b0808ad
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-11 10:27:31 +0200

    Add creation of payemnt intents after slot validation

--- backend/legal-service-app/app/Http/Controllers/AppointmentController.php
+++ backend/legal-service-app/app/Http/Controllers/AppointmentController.php
@@ -7,7 +7,10 @@ use App\Helpers\AppointmentHelper;
 use App\Http\Requests\JSONRequest;
 use App\Lawyer;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Support\Facades\Auth;
+use Stripe\PaymentIntent;
+use Stripe\Stripe;
 
 class AppointmentController extends Controller
 {
@@ -32,6 +35,8 @@ class AppointmentController extends Controller
             2) It's an actual slot for the lawyer by checking if it evenly divides the slot_length and is available in schedule
          */
         $selected_slots = $request->get('slots');
+        $total_price = 0;
+        $appointments = [];
         foreach ($selected_slots as $slot) {
             $slot_datetime = new Carbon($slot['datetime']);
             // Check 1
@@ -60,11 +65,34 @@ class AppointmentController extends Controller
                 'price' => $lawyer->price_per_slot, // TODO: Account for discount
                 'duration' => $lawyer->slot_length
             ]);
+            $total_price += $appointment->price;
             $appointment->lawyer()->associate($lawyer);
             $appointment->client()->associate($user);
-            $appointment->save();
+            $appointments[] = $appointment;
+            // $appointment->save();
             // TODO: Have a job to remove held appointments after some time
-            // Create payment intent
+        }
+
+        // Create payment intent
+        Stripe::setApiKey(config('app.stripe_api_key'));
+        try {
+            $paymentIntent = PaymentIntent::create([
+                'amount' => $total_price,
+                'currency' => 'usd'
+            ]);
+            foreach ($appointments as $appointment) {
+                $appointment->save();
+            }
+            return [
+                'error' => false,
+                'client_secret' => $paymentIntent->client_secret
+            ];
+        } catch (Exception $e) {
+            dump($e);
+            return [
+                'error' => true,
+                'message' => 'Error creating payment'
+            ];
         }
     }
 }

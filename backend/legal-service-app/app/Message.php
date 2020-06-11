1a01b62120cdde3471517452933d307497055743
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-11 04:06:02 +0200

    Native types cast for the Message model

--- backend/legal-service-app/app/Message.php
+++ backend/legal-service-app/app/Message.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     public $fillable = ['content'];
+
+    protected $casts = [
+        'is_read' => 'bool',
+    ];
+
     public function sender()
     {
         return $this->belongsTo(Account::class, 'sender_id');

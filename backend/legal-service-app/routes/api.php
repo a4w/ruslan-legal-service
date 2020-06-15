4d92fcae91ba67553bf5fec50e9a42e5057b3bfc
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-15 10:11:02 +0200

    Added payment listener webhook

--- backend/legal-service-app/routes/api.php
+++ backend/legal-service-app/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Appointment;
+use App\Http\Requests\JSONRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,3 +37,30 @@ Route::post('/account/update-password', 'AccountController@updatePassword')->mid
 
 Route::post('/appointment/{lawyer}/select-slots', 'AppointmentController@selectSlots')->middleware('auth:api');
 Route::post('/lawyer/update-schedule', 'LawyerController@updateSchedule')->middleware('auth:api');
+Route::post('/payment-status-webhook', function (JSONRequest $request) {
+    $intent_id = $request->input('data.object.id');
+    $amount = $request->input('data.object.amount');
+    $currency = $request->input('data.object.currency');
+    $status = $request->input('data.object.status');
+    $type = $request->input('type');
+    if ($type !== 'payment_intent.succeeded' || $currency !== 'gbp' || $status !== 'succeeded') {
+        return;
+    }
+    // Find appointments associated with this intent
+    /** */
+    $appointments = Appointment::where('payment_intent_id', $intent_id)->get();
+    // Verify amount
+    $total = 0;
+    foreach ($appointments as $appointment) {
+        $total += $appointment->price;
+    }
+    $total *= 100;
+    if ($amount !== $total) {
+        return;
+    }
+    // Now all is good, update appointment status
+    foreach ($appointment as $appointment) {
+        $appointment->status = 'UPCOMING';
+        $appointment->save();
+    }
+});

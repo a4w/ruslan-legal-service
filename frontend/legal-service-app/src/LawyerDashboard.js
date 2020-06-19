38b45dbceea7106e472c2f40252a5efff5d822c4
Sara Samer <ss.titanic199@gmail.com>
2020-06-20 00:17:05 +0200

    Added broswer router to dashboard

--- frontend/legal-service-app/src/LawyerDashboard.js
+++ frontend/legal-service-app/src/LawyerDashboard.js
@@ -1,24 +1,23 @@
 import React from "react";
 import LawyerDashboardSidebar from "./LawyerDashboardSidebar";
 import StickyBox from "react-sticky-box";
-import Tab from "react-bootstrap/Tab";
 import LawyerDashboardTabs from "./LawyerDashboardTabs";
+import { BrowserRouter } from "react-router-dom";
 
 const LawyerDashboard = () => {
     return (
-        <div className="row">
-            {" "}
-            <Tab.Container id="dashboard" defaultActiveKey="reviews">
+        <BrowserRouter>
+            <div className="row">
                 <div className="col-md-5 col-lg-4 col-xl-3">
                     <StickyBox offsetTop={20} offsetBottom={20}>
                         <LawyerDashboardSidebar />
                     </StickyBox>
                 </div>
-                <div className="col-md-7 col-lg-8 col-xl-9">
+                <div className="col-md-7 col-lg-8 col-xl-9 mt-4">
                     <LawyerDashboardTabs />
                 </div>
-            </Tab.Container>
-        </div>
+            </div>
+        </BrowserRouter>
     );
 };
 export default LawyerDashboard;

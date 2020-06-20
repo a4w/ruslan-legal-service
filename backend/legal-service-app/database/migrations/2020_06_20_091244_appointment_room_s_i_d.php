3fdc330931ebb54f873aff425e082cd4d0985dc9
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-20 11:16:59 +0200

    Add migration to store room sid

---
+++ backend/legal-service-app/database/migrations/2020_06_20_091244_appointment_room_s_i_d.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AppointmentRoomSID extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->string('room_sid')->index('appointments_room_sid_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->dropIndex('appointments_room_sid_index');
+            $table->dropColumn('room_sid');
+        });
+    }
+}

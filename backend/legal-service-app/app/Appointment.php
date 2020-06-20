53f3655dac2b6b77ef95e55e420ea8d4cd9f8d95
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-20 13:13:01 +0200

    Add create room method to Appointment

--- backend/legal-service-app/app/Appointment.php
+++ backend/legal-service-app/app/Appointment.php
@@ -29,4 +29,22 @@ class Appointment extends Model
     {
         return $this->hasOne(Rating::class);
     }
+
+    public function createRoom()
+    {
+        // Create the room
+        $sid    = config('app.twilio_account_sid');
+        $token  = config('app.twilio_auth_token');
+        $twilio = new Client($sid, $token);
+        $room = $twilio->video->v1->rooms
+            ->create(
+                [
+                    "enableTurn" => true,
+                    "type" => "peer-to-peer",
+                    "maxParticipants" => 2
+                ]
+            );
+        $this->room_sid = $room->sid;
+        $this->save();
+    }
 }

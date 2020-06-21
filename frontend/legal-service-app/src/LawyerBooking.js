ae590281ba1b4f185d1f7b46d162df241cf081c0
Sara Samer <ss.titanic199@gmail.com>
2020-06-21 03:42:34 +0200

    Added Appointment time from

--- frontend/legal-service-app/src/LawyerBooking.js
+++ frontend/legal-service-app/src/LawyerBooking.js
@@ -1,6 +1,7 @@
 import React from "react";
 import StarRatings from "react-star-ratings";
 import LawyerCardList from "./LawyerCardList";
+import AppointmentTimeForm from "./AppointmentTimeForm";
 
 const LawyerBooking = () => {
     return (
@@ -9,6 +10,7 @@ const LawyerBooking = () => {
                 <div class="row">
                     <div class="col-12">
                         <LawyerCard />
+                        <AppointmentTimeForm />
                     </div>
                 </div>
             </div>

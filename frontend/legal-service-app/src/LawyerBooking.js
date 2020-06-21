196421cb377132ea70be05ed49e91d8a59e912e3
Sara Samer <ss.titanic199@gmail.com>
2020-06-21 03:39:35 +0200

    Lawyer Card Added

---
+++ frontend/legal-service-app/src/LawyerBooking.js
@@ -0,0 +1,43 @@
+import React from "react";
+import StarRatings from "react-star-ratings";
+
+const LawyerBooking = () => {
+    return <LawyerCard />;
+};
+const LawyerCard = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <div className="booking-doc-info">
+                    <a href="doctor-profile.html" className="booking-doc-img">
+                        Profile Pic
+                    </a>
+                    <div className="booking-info">
+                        <h4>
+                            <a href="doctor-profile.html">Name</a>
+                        </h4>
+                        <div className="rating">
+                            <StarRatings
+                                rating={4}
+                                starRatedColor="gold"
+                                starDimension="20px"
+                                starSpacing="0px"
+                                numberOfStars={5}
+                                name="rating"
+                            />
+                            &nbsp;
+                            <span className="d-inline-block text-xs average-rating">
+                                (number of clients rated, percentage)
+                            </span>
+                        </div>
+                        <p className="text-muted mb-0">
+                            <i className="fas fa-map-marker-alt"></i> City,
+                            Country
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default LawyerBooking;

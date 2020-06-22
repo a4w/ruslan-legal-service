249ccf904d562c481e24ae85308d2e7bf1e06ec5
Sara Samer <ss.titanic199@gmail.com>
2020-06-22 20:03:24 +0200

    Initial component

---
+++ frontend/legal-service-app/src/LawyerRating.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LawyerRating = () => {
+    return (
+        <div class="content">
+            <div class="container">
+                <div className="row">test</div>
+            </div>
+        </div>
+    );
+};
+export default LawyerRating;

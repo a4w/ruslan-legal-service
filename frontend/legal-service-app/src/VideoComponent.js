2718cede9ac16c1e9b53ceff0258bf92b0545179
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-23 13:21:34 +0200

    Add basic controls

--- frontend/legal-service-app/src/VideoComponent.js
+++ frontend/legal-service-app/src/VideoComponent.js
@@ -1,16 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
 import "./assets/css/VideoComponent.css"
+import {FaCheck, FaPhoneSlash, FaVolumeMute, FaTextWidth, FaFacebookMessenger, FaMailBulk, FaPhoneVolume, FaVolumeOff} from "react-icons/fa"
+import {BsChatSquareQuote} from "react-icons/bs"
 
 
 const VideoComponent = () => {
+    const [isMuted, setIsMuted] = useState(true);
+    const handleSoundControl = () => {
+        setIsMuted(!isMuted);
+    };
+
     return (
         <>
             <div class="row no-gutters">
                 <div class="col-12 col-md-6 col-lg-8">
                     <div className="stream">
-                        <div className="outgoing">
-
+                        <div className="controls">
+                            <div className="buttons">
+                                <button class="btn btn-info"><BsChatSquareQuote /></button>
+                                <button class="btn btn-outline btn-danger"><FaPhoneSlash /></button>
+                                <button class="btn btn-primary" onClick={handleSoundControl}>{isMuted ? <FaVolumeOff /> : <FaVolumeMute />}</button>
+                            </div>
                         </div>
+                        <div className="outgoing"> </div>
                     </div>
                 </div>
                 <div class="col-12 col-md-6 col-lg-8">

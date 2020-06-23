677a82a807dfb7ff6337bf1136511b3b4a6401ec
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-23 13:07:41 +0200

    Initial component

---
+++ frontend/legal-service-app/src/VideoComponent.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "./assets/css/VideoComponent.css"
+
+
+const VideoComponent = () => {
+    return (
+        <>
+            <div class="row no-gutters">
+                <div class="col-12 col-md-6 col-lg-8">
+                    <div className="stream">
+                        <div className="outgoing">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-8">
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+
+export default VideoComponent;

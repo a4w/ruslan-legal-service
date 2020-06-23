dd7ca9d627b994a9abdabbcdbdc54754acf30553
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-23 13:36:32 +0200

    Work with controls for mobile view

--- frontend/legal-service-app/src/VideoComponent.js
+++ frontend/legal-service-app/src/VideoComponent.js
@@ -6,27 +6,31 @@ import {BsChatSquareQuote} from "react-icons/bs"
 
 const VideoComponent = () => {
     const [isMuted, setIsMuted] = useState(true);
+    const [showChat, setShowChat] = useState(false);
     const handleSoundControl = () => {
         setIsMuted(!isMuted);
+    };
+    const handleChatToggle = () => {
+        setShowChat(!showChat);
     };
 
     return (
         <>
             <div class="row no-gutters">
-                <div class="col-12 col-md-6 col-lg-8">
+                <div class="col">
                     <div className="stream">
                         <div className="controls">
                             <div className="buttons">
-                                <button class="btn btn-info"><BsChatSquareQuote /></button>
-                                <button class="btn btn-outline btn-danger"><FaPhoneSlash /></button>
+                                <button class="btn btn-info" onClick={handleChatToggle}><BsChatSquareQuote /></button>
+                                <button class="btn btn-danger"><FaPhoneSlash /></button>
                                 <button class="btn btn-primary" onClick={handleSoundControl}>{isMuted ? <FaVolumeOff /> : <FaVolumeMute />}</button>
                             </div>
                         </div>
                         <div className="outgoing"> </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-6 col-lg-8">
-
+                <div className={"col-12 col-md-6 col-lg-4 " + (showChat ? 'd-block' : 'd-none')}>
+                    Chat
                 </div>
             </div>
         </>

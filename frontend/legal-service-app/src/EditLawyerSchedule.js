8d07fe921a3e320997c84ff4f8dc63b7ace04a6c
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-24 14:18:05 +0200

    Adjust AppointmentTimeForm options

--- frontend/legal-service-app/src/EditLawyerSchedule.js
+++ frontend/legal-service-app/src/EditLawyerSchedule.js
@@ -83,7 +83,7 @@ const EditLawyerSchedule = ({}) => {
                     </StickyBox>
                 </div>
                 <div className="col-9">
-                    <AppointmentTimeForm />
+                    <AppointmentTimeForm calender={false} />
                 </div>
             </div>
         </>

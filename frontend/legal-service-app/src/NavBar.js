fc9463893110ef3f06b697a2f5a8e2f84bf0003c
Sara Samer <ss.titanic199@gmail.com>
2020-06-24 16:22:41 +0200

    Made navbar responsive with state hook :)

--- frontend/legal-service-app/src/NavBar.js
+++ frontend/legal-service-app/src/NavBar.js
@@ -5,11 +5,12 @@ import ModalPopUp from "./Modal";
 
 const NavBar = () => {
     const [modalShow, setModalShow] = useState(false);
+    const [open, setOpen] = useState(false)
     return (
         <header className="header">
             <nav className="navbar navbar-expand-lg header-nav">
                 <div className="navbar-header">
-                    <a id="mobile_btn" href="#">
+                    <a id="mobile_btn" onClick={()=> setOpen(true)} href="#">
                         <span className="bar-icon">
                             <span></span>
                             <span></span>
@@ -20,12 +21,12 @@ const NavBar = () => {
                         <b>Lawbe</b>.co.uk
                     </Link>
                 </div>
-                <div className="main-menu-wrapper">
+                <div className="main-menu-wrapper" style={{transform: open? "translateX(0px)":"translateX(-260px)"}}>
                     <div className="menu-header">
                         <Link className="menu-logo" to="/">
                             <b>Lawbe</b>.co.uk
                         </Link>
-                        <a id="menu_close" className="menu-close" href="#">
+                        <a id="menu_close" onClick={()=> setOpen(false)} className="menu-close" href="#">
                             <i className="fas fa-times"></i>
                         </a>
                     </div>

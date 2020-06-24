611a74121a18ecb8bad0736d6b03915afc18993e
Sara Samer <ss.titanic199@gmail.com>
2020-06-24 19:22:01 +0200

    Passed down hide chat list

--- frontend/legal-service-app/src/ChatUserList.js
+++ frontend/legal-service-app/src/ChatUserList.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ChatUserListItem from "./ChatUserListItem"
 
-const ChatUserList = ({chats, onChatSelection}) => {
+const ChatUserList = ({chats, onChatSelection, swipe}) => {
     return (
         <>
             <div className="chat-users-list">
                 <div className="chat-scroll">
                     {chats.map((chat, i) => {
-                        return (<ChatUserListItem key={chat.id} chat_index={i} other_name={chat.other_name} handleClick={onChatSelection} />);
+                        return (
+                            <ChatUserListItem
+                                key={chat.id}
+                                chat_index={i}
+                                other_name={chat.other_name}
+                                handleClick={onChatSelection}
+                                swipe={swipe}
+                            />
+                        );
                     })}
                 </div>
             </div>
--- frontend/legal-service-app/src/ChatUserListItem.js
+++ frontend/legal-service-app/src/ChatUserListItem.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const ChatUserListItem = ({chat_index, other_name, handleClick}) => {
+const ChatUserListItem = ({swipe, chat_index, other_name, handleClick}) => {
     return (
-        <div className="media" onClick={() => {handleClick(chat_index)}}>
+        <div
+            className="media"
+            onClick={() => {
+                handleClick(chat_index);
+                swipe();
+            }}
+        >
             <div className="media-body">
                 <div>
                     <div className="user-name">{other_name}</div>

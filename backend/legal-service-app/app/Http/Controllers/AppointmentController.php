01b75fa6802e3b84a048e9d49a42f221b7d64542
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-24 10:41:36 +0200

    Fix automatically closed rooms

--- backend/legal-service-app/app/Http/Controllers/AppointmentController.php
+++ backend/legal-service-app/app/Http/Controllers/AppointmentController.php
@@ -112,7 +112,10 @@ class AppointmentController extends Controller
         // Verify room is present
         $twilio = resolve(\Twilio\Rest\Client::class);
         try {
-            $twilio->video->rooms($appointment->room_sid)->fetch();
+            $room = $twilio->video->rooms($appointment->room_sid)->fetch();
+            if ($room->status === "completed") {
+                throw new Exception("Room too old");
+            }
         } catch (Exception $e) {
             // Recreate room
             $appointment->createRoom();

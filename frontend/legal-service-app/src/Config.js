faae98f26ad9117b0d0bfdb40ee52b06cf8cdb7f
Sara Samer <ss.titanic199@gmail.com>
2020-06-27 16:10:38 +0200

    Google client id in config

--- frontend/legal-service-app/src/Config.js
+++ frontend/legal-service-app/src/Config.js
@@ -2,6 +2,7 @@ const Config = {
     // api_url: "http://testingapi.lawbe.co.uk",
     api_url: "http://127.0.0.1:8000/api",
     momentsjs_default_datetime_format: "Y-MM-DD HH:mm:ss",
-    momentsjs_default_date_format: "Y-MM-DD"
+    momentsjs_default_date_format: "Y-MM-DD",
+    google_client_id:"xxx",
 };
 export default Config;

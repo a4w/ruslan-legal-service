d010b9be6903be481e2c024da882e0638ee8ef28
Sara Samer <ss.titanic199@gmail.com>
2020-06-27 16:12:53 +0200

    Used doccure button styling

--- frontend/legal-service-app/src/GoogleButton.js
+++ frontend/legal-service-app/src/GoogleButton.js
@@ -2,15 +2,23 @@ import React from "react";
 import GoogleLogin from "react-google-login";
 import Config from "./Config";
 
-
-const GoogleButton = ({register}) => {
+const GoogleButton = ({ register }) => {
     const responseGoogle = (response) => {
         console.log(response);
     };
     return (
         <GoogleLogin
             clientId={Config.google_client_id}
-            buttonText={register ? " Register" : " Login"}
+            render={(renderProps) => (
+                <a
+                    href="//"
+                    className="btn btn-google btn-block"
+                    onClick={renderProps.onClick}
+                >
+                    <i className="fab fa-google mr-1"></i>{" "}
+                    {register ? " Register" : " Login"}
+                </a>
+            )}
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
             cookiePolicy={"single_host_origin"}

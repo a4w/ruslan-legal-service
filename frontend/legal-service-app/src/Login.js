601e89eedcaa5c2b8e432131bd17dc922c6c5610
Sara Samer <ss.titanic199@gmail.com>
2020-06-27 21:47:56 +0200

    Login modal

---
+++ frontend/legal-service-app/src/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import history from "./History";
+import Modal from "./ModalRouted";
+
+const LoginModal = (props) => {
+    return (
+        <Modal
+            onClick={() => {
+                history.push(props.match.url);
+            }}
+        >
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    height: "100%",
+                }}
+            >
+                <LoginForm />
+            </div>
+        </Modal>
+    );
+};
+
+export default LoginModal;

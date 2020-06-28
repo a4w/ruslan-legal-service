83d2cd28328eca3dd044ec6bb0dea08a6bcf2763
Sara Samer <ss.titanic199@gmail.com>
2020-06-28 06:53:44 +0200

    Fixed styling and outside click bug

--- frontend/legal-service-app/src/ModalRouted.js
+++ frontend/legal-service-app/src/ModalRouted.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { createPortal } from "react-dom";
+import History from "./History";
 
 const modalStyle = {
     position: "fixed",
@@ -8,14 +9,63 @@ const modalStyle = {
     bottom: 0,
     right: 0,
     backgroundColor: "rgba(0,0,0,.2)",
+    alignItems: "center",
+    justifyContent: "center",
     color: "##FFF",
+    display: "flex"
 };
-const Modal = (props) => {
+const ModalPortal = (props) => {
     return createPortal(
-        <div style={modalStyle} onClick={props.onClick}>
+        <div
+            style={modalStyle}
+            onClick={props.onClick}
+        >
             {props.children}
         </div>,
         document.getElementById("modal_root")
+    );
+};
+
+const Modal = (props) => {
+    const ref = useRef(null);
+    const [clicked, setClickedOutside] = useState(false);
+
+    const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setClickedOutside(true);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutside);
+    }, [ref]);
+
+    const OnClick = () => {
+        if (clicked) History.goBack();
+    };
+    return (
+        <ModalPortal onClick={OnClick}>
+            <div
+                style={{
+                    position: "relative",
+                    display: "flex",
+                    flexDirection: "column",
+                    pointerEvents: "auto",
+                    backgroundColor: "#fff",
+                    backgroundClip: "padding-box",
+                    border: "1px solid rgba(0,0,0,.2)",
+                    borderRadius: ".3rem",
+                    outline: "0",
+                    width: "90%",
+                    margin: "auto",
+                }}
+                ref={ref}
+            >
+                {props.children}
+            </div>
+        </ModalPortal>
     );
 };
 

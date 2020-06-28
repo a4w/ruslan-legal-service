7bf129910be825daf407581a732153ba2b9f0898
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-28 07:57:04 +0200

    Accessors for media messages

--- backend/legal-service-app/app/Message.php
+++ backend/legal-service-app/app/Message.php
@@ -12,6 +12,34 @@ class Message extends Model
         'is_read' => 'bool',
     ];
 
+    protected $appends = ['link'];
+
+    public function getLinkAttribute()
+    {
+        if ($this->message_type === 'FILE') {
+            return route('chat.download_file', ['mid' => $this->id]);
+        }
+        return null;
+    }
+
+    public function getUIDAttribute()
+    {
+        if ($this->message_type === 'FILE') {
+            $content = json_decode($this->attributes['content']);
+            return $content->uid;
+        }
+        return null;
+    }
+
+    public function getContentAttribute($content)
+    {
+        if ($this->message_type === 'FILE') {
+            $content = json_decode($content);
+            return $content->name;
+        }
+        return $content;
+    }
+
     public function sender()
     {
         return $this->belongsTo(Account::class, 'sender_id');

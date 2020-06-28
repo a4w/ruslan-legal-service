090da84b980638251dc165740e970784c477b63c
Sara Samer <ss.titanic199@gmail.com>
2020-06-28 20:01:37 +0200

    Made wrapper component

--- frontend/legal-service-app/src/WriteBlog.js
+++ frontend/legal-service-app/src/WriteBlog.js
@@ -1,13 +1,15 @@
-import React, { useRef, useEffect } from "react";
+import React, {useRef, useState} from "react";
 import Stackedit from "stackedit-js";
 import { FaPencilAlt } from "react-icons/fa";
+import ErrorMessageInput from "./ErrorMessageInput";
+import Select from "react-dropdown-select";
 
 const EditStyles = {
     backgroundColor: "#2c2c2c",
     padding: "5px",
     textAlign: "center",
     width: "100%",
-    height:"60px",
+    height: "60px",
 };
 
 const ButtonStyles = {
@@ -96,4 +98,81 @@ This is **bold**,  _italic_ and ~~strikethrough text~~.
     );
 };
 
-export default WriteBlog;
+const BlogPage = () => {
+    const [coverData, setCoverData] = useState({ cover: "", coverFile: "" });
+    const [title, setTitle] = useState("");
+    const showSelectedCover = (e) => {
+        const input = e.target;
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setCoverData({
+                    cover: e.target.result,
+                    coverFile: input.files[0],
+                });
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    };
+    const dateString = (new Date()).toLocaleString("en-GB", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+    const tags = [
+        { value: "1", label: "tag 1" },
+        { value: "1", label: "tag 2" },
+        { value: "1", label: "tag 3" },
+        { value: "1", label: "tag 4" },
+    ];
+    return (
+        <div className="blog blog-single-post">
+        <div className="blog-image">
+            <img alt="Cover" src={coverData.cover} className="img-fluid" />
+        </div>
+        <h3 className="blog-title">
+            <ErrorMessageInput
+                placeholder="Title.."
+                type="text"
+                OnChangeHandler={({ target: { value } }) => setTitle(value)}
+                value={title}
+            />
+        </h3>
+        <div className="blog-info clearfix">
+            <div className="post-left">
+                <ul>
+                    <li>
+                        <div className="post-author">
+                            <div className="change-photo-btn">
+                                <span>
+                                    <i className="fa fa-upload"></i> Upload
+                                    Photo
+                                </span>
+                                <input
+                                    type="file"
+                                    accept="image/png,image/jpeg,image/jpg,image/gif"
+                                    onChange={showSelectedCover}
+                                    className="upload"
+                                />
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <i className="far fa-calendar"></i>
+                        {dateString}
+                    </li>
+                    <li style={{display:"block ruby"}}>
+                        <i className="fa fa-tags"></i>{" "}
+                        <Select options={tags} multi />
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div className="blog-content">
+          <WriteBlog />
+          </div>
+    </div>
+);
+};
+
+export default BlogPage;

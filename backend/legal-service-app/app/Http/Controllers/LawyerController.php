c81aa5713f94b2f56c79ee7a261251f41f6eea25
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-29 12:57:20 +0200

    Lawyer exist check

--- backend/legal-service-app/app/Http/Controllers/LawyerController.php
+++ backend/legal-service-app/app/Http/Controllers/LawyerController.php
@@ -43,7 +43,7 @@ class LawyerController extends Controller
     {
         // Get lawyer
         $lawyer = Lawyer::find($id);
-        if (!$lawyer->isAvailable()) {
+        if ($lawyer !== null && !$lawyer->isAvailable()) {
             return RespondJSON::unknownError();
         }
         $schedule = $lawyer->schedule;

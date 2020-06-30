a88ec501e32d4b56449c9c4e3ad18564397f80dc
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-30 18:12:42 +0200

    Implement add blog endpoint

--- backend/legal-service-app/app/Http/Controllers/BlogsController.php
+++ backend/legal-service-app/app/Http/Controllers/BlogsController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Blog;
 use App\Helpers\RespondJSON;
+use App\Http\Requests\JSONRequest;
+use App\PracticeArea;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BlogsController extends Controller
 {
@@ -18,5 +21,26 @@ class BlogsController extends Controller
     {
         $blogs = $lawyer->blogs;
         return RespondJSON::success(['blogs' => $blogs]);
+    }
+
+    public function addBlogPost(JSONRequest $request)
+    {
+        $request->validate([
+            'title' => ['required', 'max:255'],
+            'body' => ['required'],
+            'tag_id' => ['required', 'exists:practice_areas,id']
+        ]);
+        /** @var Account */
+        $user = Auth::user();
+        if (!$user->isLawyer()) {
+            return RespondJSON::forbidden();
+        }
+        $lawyer = $user->lawyer;
+        $blog = Blog::make($request->only(['title', 'body']));
+        $blog->cover_photo_path = null;
+        $practice_area = PracticeArea::find($request->get('tag_id'));
+        $blog->tag()->associate($practice_area);
+        $blog->lawyer()->associate($lawyer);
+        $blog->save();
     }
 }

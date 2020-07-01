771adc7388bdbf2f7410ea45a4eba22d8bcae1b8
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-07-01 16:38:12 +0200

    Create message received notification

---
+++ backend/legal-service-app/app/Notifications/MessageReceived.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Message;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
+
+class MessageReceived extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected Message $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+
+    public function toArray($notifiable)
+    {
+        return [
+            'type' => 'INCOMING_MESSAGE',
+            'data' => [
+                'message_hint' => Str::substr($this->message->content, 0, 20),
+                'sender_name' => $this->message->sender->name . ' ' . $this->message->sender->surname
+            ]
+        ];
+    }
+}

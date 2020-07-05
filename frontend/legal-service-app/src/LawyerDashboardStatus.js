82ab83eba57dfa84218dd53719a323a600a70fb4
Sara Samer <ss.titanic199@gmail.com>
2020-07-05 22:28:51 +0200

    added view to list item in lawyer dashboard

--- frontend/legal-service-app/src/LawyerDashboardStatus.js
+++ frontend/legal-service-app/src/LawyerDashboardStatus.js
@@ -8,6 +8,7 @@ import {request} from "./Axios";
 import {toast} from "react-toastify";
 import {FaUser, FaCalendar, FaRegCalendarPlus, FaCalendarCheck} from "react-icons/fa";
 import Img from "./Img";
+import History from "./History";
 
 const LawyerDashboardStatus = () => {
     return (
@@ -182,6 +183,9 @@ const ListItem = ({appointment}) => {
                             <i className="fas fa-times"></i> Cancel
                         </button>
                     )}
+                    <Link to={`${History.location.pathname}/details/${appointment.id}`} className="btn btn-sm bg-info-light">
+                        <i className="far fa-eye"></i> View
+                    </Link>
                 </div>
             </td>
         </tr>

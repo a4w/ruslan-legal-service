4d07c6fe73b9c8fd310a29bcffa5e733a50a2726
Sara Samer <ss.titanic199@gmail.com>
2020-07-06 01:41:00 +0200

    Added img fallback

--- frontend/legal-service-app/src/Img.js
+++ frontend/legal-service-app/src/Img.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 const Img = ({src, className, alt, style}) => {
+    const OnError = (e) => {
+        e.target.src = "/undraw_male_avatar.svg";
+        e.target.onError = null;
+    };
     return (
         <img
-            src={src ? src : "/undraw_male_avatar.svg"}
+            src={src ? src : ""}
             className={className ? className : ""}
             alt={alt ? alt : "img"}
             style={style}
+            onError={OnError}
         />
     );
 }

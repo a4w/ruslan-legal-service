716d8d1925400925a6be2c1c2dd15bbb389a8795
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-07-06 18:28:50 +0200

    Fix reviews tab by passing the correct account object

--- frontend/legal-service-app/src/LawyerReviews.js
+++ frontend/legal-service-app/src/LawyerReviews.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import StarRatings from "react-star-ratings";
 import moment from "moment";
-import { request } from "./Axios";
+import {request} from "./Axios";
 import Img from "./Img";
 
 const LawyerReviews = ({lawyer}) => {
     const [comments, setComments] = useState(null);
     useEffect(() => {
         if (!lawyer)
-            request({ url: "/lawyer/me", method: "GET" })
+            request({url: "/lawyer/me", method: "GET"})
                 .then((data) => {
                     setComments(data.lawyer.ratings);
                 })
@@ -18,13 +18,13 @@ const LawyerReviews = ({lawyer}) => {
     return <ReviewList comments={comments} />;
 };
 
-const ReviewList = ({ comments }) => {
+const ReviewList = ({comments}) => {
     return (
         <div className="lawyer-review review-listing">
             <ul className="comments-list">
                 {comments && comments.map((comment) => (
                     <li key={comment.id}>
-                        <Comment comment={comment} />
+                        <Comment comment={comment} account={comment.appointment.client.account} />
                     </li>
                 ))}
             </ul>
@@ -32,8 +32,8 @@ const ReviewList = ({ comments }) => {
     );
 };
 const Comment = ({comment, account = {}}) => {
-    const date = moment(comment.created_at).format("MMMM Do YYYY, hh:mm a"); 
-    
+    const date = moment(comment.created_at).format("MMMM Do YYYY, hh:mm a");
+
     return (
         <div className="comment">
             <Img
@@ -41,7 +41,7 @@ const Comment = ({comment, account = {}}) => {
                 alt="User"
                 src={account.profile_picture}
             />
-            <div className="comment-body" style={{ width: "100%" }}>
+            <div className="comment-body" style={{width: "100%"}}>
                 <div className="meta-data">
                     <span className="comment-author">{`${account.name} ${account.surname}`}</span>
                     <span className="comment-date">{date}</span>
@@ -62,11 +62,11 @@ const Comment = ({comment, account = {}}) => {
                         Lawyer
                     </p>
                 ) : (
-                    <p className="not-recommended">
-                        <i className="far fa-thumbs-down"></i> I don't recommend
+                        <p className="not-recommended">
+                            <i className="far fa-thumbs-down"></i> I don't recommend
                         this Lawyer
-                    </p>
-                )}
+                        </p>
+                    )}
                 <p className="comment-content">{comment.comment}</p>
             </div>
         </div>

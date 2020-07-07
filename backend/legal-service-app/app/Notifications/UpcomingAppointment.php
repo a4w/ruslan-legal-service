54776f162cb38ecce0bd50dd30a4c63fd01ad2db
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-07-07 13:03:40 +0200

    Create upcoming appointment notification and dispatcher

--- backend/legal-service-app/app/Notifications/UpcomingAppointment.php
+++ backend/legal-service-app/app/Notifications/UpcomingAppointment.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UpcomingAppoitment extends Notification
+class UpcomingAppointment extends Notification
 {
     use Queueable;
 

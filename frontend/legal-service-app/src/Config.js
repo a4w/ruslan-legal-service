5617d99f12099648c2e418a16e4ca1977882d68b
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-07-09 01:11:03 +0200

    Config file for backend

--- frontend/legal-service-app/src/Config.js
+++ frontend/legal-service-app/src/Config.js
@@ -1,6 +1,6 @@
 const Config = {
     // api_url: "http://testingapi.lawbe.co.uk",
-    api_url: "http://127.0.0.1:8000/api",
+    api_url: "https://testingapi.lawbe.co.uk/api",
     momentsjs_default_datetime_format: "Y-MM-DD HH:mm:ss",
     momentsjs_default_date_format: "Y-MM-DD",
     facebook_app_id: "1349693598562280",

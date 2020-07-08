7b5914d6f235f4ea41b0b86985d6daba36093eed
Sara Samer <ss.titanic199@gmail.com>
2020-07-08 18:25:06 +0200

    Fixed search lawyer by name unwanted refresh

--- frontend/legal-service-app/src/Home.js
+++ frontend/legal-service-app/src/Home.js
@@ -232,7 +232,12 @@ const SearchLawyerByName = () => {
                     </div>
                 </div>
                 <div className="col-auto">
-                    <button type="submit" style={{height: '46px'}} className="btn btn-block btn-primary search-btn">
+                    <button
+                        type="submit"
+                        style={{ height: "46px" }}
+                        className="btn btn-block btn-primary search-btn"
+                        onClick={(e) => e.preventDefault()}
+                    >
                         <i className="fas fa-search"></i>
                     </button>
                 </div>

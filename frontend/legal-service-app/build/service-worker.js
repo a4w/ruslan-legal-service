6ba6a47b0025dc7bb790fef52a536ebcb781fe32
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-07-09 14:52:59 +0200

    Build with different base path

--- frontend/legal-service-app/build/service-worker.js
+++ frontend/legal-service-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6588599118fd8055021e772535b90994.js"
+  "/precache-manifest.367147e09344a25b3fb49eafa113980a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

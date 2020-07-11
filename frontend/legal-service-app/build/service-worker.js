a6d586a02599b0841cfbd915e4b30b38c0a391f9
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-07-11 22:32:32 +0200

    NPM react build

--- frontend/legal-service-app/build/service-worker.js
+++ frontend/legal-service-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50cf2339e35354088850714dd134fad1.js"
+  "./precache-manifest.36c48b0af7cee3660d3e636d701abf8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

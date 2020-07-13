b3e29db483601cb4b455c0ba9165e789ed0c9ff7
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-07-13 13:59:40 +0200

    Private route new auth

--- frontend/legal-service-app/src/PrivateRoute.js
+++ frontend/legal-service-app/src/PrivateRoute.js
@@ -1,11 +1,10 @@
-import React from "react"
-import Cookies from "universal-cookie";
+import React, {useContext} from "react"
 import {Route, Redirect} from "react-router-dom";
-
-const cookie = new Cookies();
+import {AuthContext} from "./App";
 
 function PrivateRoute({component, ...rest}) {
-    if (cookie.get("logged_in")) {
+    const [auth,] = useContext(AuthContext);
+    if (auth.isLoggedIn) {
         return <Route {...rest} component={component} />;
     } else {
         return <Route {...rest} render={() => (<Redirect to="/home/login" />)} />

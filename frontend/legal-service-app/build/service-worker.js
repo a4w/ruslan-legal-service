aab03b1c172359fcfb3a29dc057e3037a8484406
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-07-21 16:38:17 +0200

    NPM build

--- frontend/legal-service-app/build/service-worker.js
+++ frontend/legal-service-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f665e401b69f64f08a0d65f88139449.js"
+  "/precache-manifest.d61d149fb617b4f76be67237d812ff84.js"
 );
 
 self.addEventListener('message', (event) => {

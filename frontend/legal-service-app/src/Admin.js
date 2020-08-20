80707ae52748e609f267ea949e8aa7d900b179bd
Sara Samer <ss.titanic199@gmail.com>
2020-08-20 05:48:32 +0200

    Made sidebar close on outside click

--- frontend/legal-service-app/src/Admin.js
+++ frontend/legal-service-app/src/Admin.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./assets/css/admin.css";
 
 const Admin = () => {
     const [sidebarToggle, setToggle] = useState(false);
+    const ref = useRef(null);
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutside);
+    }, [ref]);
+
+    const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setToggle(false);
+        }
+    };
+
     const handleToggle = () => setToggle(!sidebarToggle);
     return (
-        <div className={`main-wrapper ${sidebarToggle? "slide-nav":""}`}>
+        <div className={`main-wrapper ${sidebarToggle ? "slide-nav" : ""}`}>
             <div className="header">
                 <div className="header-left">
                     <a href="index.html" className="logo">
@@ -22,7 +35,9 @@ const Admin = () => {
                     <i className="fa fa-bars"></i>
                 </a>
             </div>
-            <AdminSidebar />
+            <div className="sidebar" id="sidebar" ref={ref}>
+                <AdminSidebar close={() => setToggle(false)} />
+            </div>
             <div className="page-wrapper">
                 <div className="content container-fluid"></div>
             </div>
@@ -32,63 +47,60 @@ const Admin = () => {
 
 const AdminSidebar = () => {
     return (
-        <div className="sidebar" id="sidebar">
-            <div className="sidebar-inner slimscroll">
-                <div id="sidebar-menu" className="sidebar-menu">
-                    <ul>
-                        <li className="active">
-                            <a href="index.html">
-                                <i className="fe fe-home"></i>{" "}
-                                <span>Dashboard</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="appointment-list.html">
-                                <i className="fe fe-layout"></i>{" "}
-                                <span>Appointments</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="specialities.html">
-                                <i className="fe fe-users"></i>{" "}
-                                <span>Specialities</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="doctor-list.html">
-                                <i className="fe fe-user-plus"></i>{" "}
-                                <span>Doctors</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="patient-list.html">
-                                <i className="fe fe-user"></i>{" "}
-                                <span>Patients</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="reviews.html">
-                                <i className="fe fe-star-o"></i>{" "}
-                                <span>Reviews</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="transactions-list.html">
-                                <i className="fe fe-activity"></i>{" "}
-                                <span>Transactions</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="settings.html">
-                                <i className="fe fe-vector"></i>{" "}
-                                <span>Settings</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="invoice-report.html">Invoice Reports</a>
-                        </li>
-                    </ul>
-                </div>
+        <div className="sidebar-inner slimscroll">
+            <div id="sidebar-menu" className="sidebar-menu">
+                <ul>
+                    <li className="active">
+                        <a href="index.html">
+                            <i className="fe fe-home"></i>{" "}
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="appointment-list.html">
+                            <i className="fe fe-layout"></i>{" "}
+                            <span>Appointments</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="specialities.html">
+                            <i className="fe fe-users"></i>{" "}
+                            <span>Specialities</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="doctor-list.html">
+                            <i className="fe fe-user-plus"></i>{" "}
+                            <span>Doctors</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="patient-list.html">
+                            <i className="fe fe-user"></i> <span>Patients</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="reviews.html">
+                            <i className="fe fe-star-o"></i>{" "}
+                            <span>Reviews</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="transactions-list.html">
+                            <i className="fe fe-activity"></i>{" "}
+                            <span>Transactions</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="settings.html">
+                            <i className="fe fe-vector"></i>{" "}
+                            <span>Settings</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="invoice-report.html">Invoice Reports</a>
+                    </li>
+                </ul>
             </div>
         </div>
     );

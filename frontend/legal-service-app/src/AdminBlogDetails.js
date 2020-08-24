46368103fc32f63106fe1187a2f17239e196d0b5
Sara Samer <ss.titanic199@gmail.com>
2020-08-25 01:19:48 +0200

    New blogs details

---
+++ frontend/legal-service-app/src/AdminBlogDetails.js
@@ -0,0 +1,8 @@
+import React from "react";
+import BlogDetails from "./BlogDetails";
+
+const AdminBlogDetails = (props) => {
+    return <BlogDetails {...props} />;
+};
+
+export default AdminBlogDetails;

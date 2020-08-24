360f2a51b77aa93b4feecbbd2a2382142531b74b
Sara Samer <ss.titanic199@gmail.com>
2020-08-25 01:41:38 +0200

    Added Publish and reject options

--- frontend/legal-service-app/src/AdminBlogDetails.js
+++ frontend/legal-service-app/src/AdminBlogDetails.js
@@ -1,8 +1,31 @@
 import React from "react";
 import BlogDetails from "./BlogDetails";
+import StickyBox from "react-sticky-box";
+import SpinnerButton from "./SpinnerButton";
+import "./AdminBlogDetails.css";
 
 const AdminBlogDetails = (props) => {
-    return <BlogDetails {...props} />;
+    return (
+        <>
+            <StickyBox offsetTop={85} style={{ zIndex: 99 }}>
+                <div className="blog-publish">
+                    <SpinnerButton
+                        type="button"
+                        className="btn btn-sm btn-rounded btn-success mr-2"
+                    >
+                        Publish
+                    </SpinnerButton>
+                    <SpinnerButton
+                        type="button"
+                        className="btn btn-sm btn-rounded btn-danger"
+                    >
+                        Reject
+                    </SpinnerButton>
+                </div>
+            </StickyBox>
+            <BlogDetails {...props} />
+        </>
+    );
 };
 
 export default AdminBlogDetails;

7e4067b307fe2408da8f9136ca08e086d3a0a7d6
Sara Samer <ss.titanic199@gmail.com>
2020-09-07 23:11:37 +0200

    Added validation and login request

--- frontend/legal-service-app/src/AdminLogin.js
+++ frontend/legal-service-app/src/AdminLogin.js
@@ -1,22 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Img from "./Img";
 import ErrorMessageInput from "./ErrorMessageInput";
+import {loginValidation} from "./Validations";
+import useValidation from "./useValidation";
+import useRequests from "./useRequests";
+import {AuthContext} from "./App";
+import SpinnerButton from "./SpinnerButton";
 
 const AdminLogin = () => {
-    const [admin, setAdmin] = useState({ username: "", password: "" });
+    const [admin, setAdmin] = useState({ email: "", password: "" });
+    const [isLoggingIn, setLoggingIn] = useState(false);
+    const [errors, addError, runValidation] = useValidation(loginValidation);
+    const {request} = useRequests();
+    const [auth, setAuth] = useContext(AuthContext);
     const onChange = ({ target: { name, value } }) => {
         const newAdmin = { ...admin, [name]: value };
         setAdmin(newAdmin);
+        runValidation(newAdmin, name);
     };
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log(admin);
+        runValidation(admin).then((hasErrors, _) => {
+            if (!hasErrors) {
+                setLoggingIn(true);
+                const url = "/admin/login";
+                request({url: url, method: "POST", data: admin})
+                    .then((data) => {
+                        console.log(data);
+                        setAuth({
+                            accessToken: data.access_token,
+                            accountType: data.account_type,
+                            refreshToken: data.refresh_token || null,
+                            isLoggedIn: true,
+                            accountId: data.user_id
+                        });
+                    })
+                    .catch((_errors) => {
+                        console.log("failed", _errors);
+                        addError(["password", "email"], {
+                            password: ["Invalid password supplied"],
+                        });
+                    })
+                    .finally(() => {
+                        setLoggingIn(false);
+                    });
+            }
+        });
     };
     return (
         <div className="main-wrapper login-body">
             <div className="login-wrapper">
                 <div className="container">
-                    <div className="loginbox">
+                    <div className="loginbox" style={{minHeight: "auto"}}>
                         <div className="login-right-wrap justify-content-center p-4 w-100">
                             <h1 className="text-center">Login</h1>
                             <p className="account-subtitle">
@@ -27,13 +62,14 @@ const AdminLogin = () => {
                                     <ErrorMessageInput
                                         className="form-control"
                                         type="email"
-                                        placeholder="Username"
-                                        value={admin.username}
+                                        placeholder="Email"
+                                        value={admin.email}
                                         OnChangeHandler={onChange}
-                                        name="username"
+                                        name="email"
+                                        errors={errors.email}
                                     />
                                 </div>
-                                <div className="form-group">
+                                <div className="form-group mb-4">
                                     <ErrorMessageInput
                                         className="form-control"
                                         type="password"
@@ -41,15 +77,17 @@ const AdminLogin = () => {
                                         value={admin.password}
                                         OnChangeHandler={onChange}
                                         name="password"
+                                        errors={errors.password}
                                     />
                                 </div>
                                 <div className="form-group">
-                                    <button
+                                    <SpinnerButton
                                         className="btn btn-primary btn-block"
                                         type="submit"
+                                        loading={isLoggingIn}
                                     >
                                         Login
-                                    </button>
+                                    </SpinnerButton>
                                 </div>
                             </form>
                         </div>

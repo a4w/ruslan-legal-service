3181ea25866fd443b47be0e3a77793be72e5442a
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-09-27 08:20:08 +0200

    Migration to fix languages

---
+++ backend/legal-service-app/database/migrations/2020_09_27_061910_fix_lawyer_languages.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixLawyerLanguages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lawyers', function (Blueprint $table) {
+            $table->dropColumn('languages');
+        });
+        Schema::table('lawyers', function (Blueprint $table) {
+            $table->json('languages')->default('[]');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lawyers', function (Blueprint $table) {
+            $table->dropColumn('languages');
+        });
+    }
+}
